0988db2c57bb3616f52bcfe95ba85fdac90f92da
vikram <vikram@dontemail.com>
2019-07-11 21:45:33 +0530

    removed unwanted header

--- ehgu_examples/time_del_fsm/tb.sv
+++ ehgu_examples/time_del_fsm/tb.sv
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.06.2019 13:36:40
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module tb(
 
     );
