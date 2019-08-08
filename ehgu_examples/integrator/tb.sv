1a9e9a86029d68c27a2be25fb985a10c765eae87
vikram <vikram@dontemail.com>
2019-08-08 07:50:06 +0530

    integrator working, may be DC drift

--- ehgu_examples/integrator/tb.sv
+++ ehgu_examples/integrator/tb.sv
@@ -32,17 +32,49 @@ end
 initial begin
 	forever begin
 		#20ps;
-		$display("Input %1.3f Step %1.3e Integral %1.3e Current time %t",ana_in,step, integral,$realtime());
+//		$display("Input %1.3f Step %1.3e Integral %1.3e Current time %t",ana_in,step, integral,$realtime());
 	end
 end
 
+      import thee_mathsci_consts_pkg::const_pi; 
+      real angle_rad;
+int LUT_SIZE = 128;
+
 initial begin
+	logic input_type = 1; 
+	if ( input_type == 0 ) begin
 	ana_in = 0 ;
 	#0.9ns;
 	rstn = 0; 
 	#1.9ns;
 	rstn = 1; ana_in = 1;
 	#20ns;
+	end else begin
+      for ( int i = 0 ; i < LUT_SIZE ; i=(i+1)%LUT_SIZE ) begin
+        angle_rad = i*2*const_pi/LUT_SIZE;
+        ana_in = $sin(angle_rad);
+        #50ps;
+      end
+	end
+end
+
+initial begin
+	#100ns;
+	$finish;
+end
+
+realtime crossing, prev_crossing, wave_period;
+real prev_integral;
+initial begin
+	forever @(integral) begin
+		if ( integral > 0 && prev_integral <= 0 ) begin
+			prev_crossing = crossing;
+			crossing = $realtime();
+			wave_period = crossing - prev_crossing ;
+			$display ( "Wave period %t", wave_period);
+		end
+		prev_integral = integral;
+	end
 end
 
 endmodule
