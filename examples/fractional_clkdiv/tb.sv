f194841ff80c518062d6b5f5e6cee20d0d8fb3af
vikram <vikram@dontemail.com>
2019-08-18 16:18:00 +0530

    window size expanded

--- examples/fractional_clkdiv/tb.sv
+++ examples/fractional_clkdiv/tb.sv
@@ -4,22 +4,27 @@ import thee_utils_pkg::check_approx_equality;
 logic clkin;
 logic clkout0;
 logic clkout1;
-real fout0,fout1;
+real fout0,exp_fout;
 bit result0,result1;
 logic rstn;
 
+localparam INT_DIVISION = 7;
+localparam FRAC_DIVISION = 14;
+localparam INT_WIDTH=3;
+localparam FRAC_WIDTH=4;
+
 thee_clk_gen_module clk_gen (.clk(clkin));
 
-thee_clk_freq_meter #(.MEAS_WINDOW(4)) fmeter0  (.clk(clkout0),.freq_in_hertz(fout0));
+thee_clk_freq_meter #(.MEAS_WINDOW(50)) fmeter0  (.clk(clkout0),.freq_in_hertz(fout0));
 //thee_clk_freq_meter fmeter1  (.clk(clkout1),.freq_in_hertz(fout1));
 
-ehgu_clkdiv_fractional clkdiv0
+ehgu_clkdiv_fractional #(.INT_WIDTH(INT_WIDTH), .FRAC_WIDTH(FRAC_WIDTH)) clkdiv0
 (
  .clkin ,
  .rstn ,
  .en (1'b1),
- .int_div(3),
- .frac_div(1),
+ .int_div(INT_DIVISION),
+ .frac_div(FRAC_DIVISION),
  .clkout (clkout0)
 );
 
@@ -28,9 +33,10 @@ initial begin
   rstn=0;
   repeat (10) @(posedge clkin);
   rstn=1;  
-  repeat (50) @(posedge clkout0);
-  $display ( " Clkout frequencies 0 %e , 1 %e", fout0,fout1);
-  check_approx_equality (.inp(fout0),.expected(1.0e9/3.25),.result(result0));
+  repeat (500) @(posedge clkout0);
+  exp_fout = 1.0e9/(INT_DIVISION + 1.0*FRAC_DIVISION/(2**FRAC_WIDTH));
+  $display ( " Clkout frequencies 0 %e , expected %e", fout0, exp_fout);
+  check_approx_equality (.inp(fout0),.expected(exp_fout),.result(result0));
   if ( result0==1) begin
     repeat (3) $display ( "PASS");
   end else begin
