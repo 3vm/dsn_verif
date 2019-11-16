7ee4e8332c77615b7296f7fe97ff896ed0f9c709
3vm <3vm@dontemail.com>
2019-11-16 12:40:46 +0530

    bug syntax error number format

---
+++ examples/pipelining_counter/counter_pipelined.sv
@@ -0,0 +1,28 @@
+
+module counter_pipelined
+#(
+parameter WIDTH=64
+)(
+input logic clk,
+input logic rstn,
+output logic [WIDTH-1:0] cnt_pipd_out
+);
+
+logic [WIDTH/2-1:0] cnt_pipd_out_halfs[2];
+logic future_carry ;
+
+always @(posedge clk) begin
+	if (!rstn) begin
+		future_carry <= 0 ;
+		cnt_pipd_out_halfs[0] <= 0 + 1 ;
+		cnt_pipd_out[WIDTH/2-1:0] <= 0 ;
+		cnt_pipd_out[WIDTH-1:WIDTH/2] <= 0 ;
+	end else begin
+		future_carry <= (cnt_pipd_out_halfs[0] + 2'b2 ) >> WIDTH/2;
+		cnt_pipd_out_halfs[0] <= cnt_pipd_out[WIDTH/2-1:0] + 1 ;
+		cnt_pipd_out[WIDTH/2-1:0] <= cnt_pipd_out_halfs[0];
+		cnt_pipd_out[WIDTH-1:WIDTH/2] <= cnt_pipd_out[WIDTH-1:WIDTH/2]+future_carry;
+	end
+end
+
+endmodule
