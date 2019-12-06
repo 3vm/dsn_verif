800dc5bc923e05bb32313c954ccdd83fc617b175
3vm <3vm@dontemail.com>
2019-12-06 09:29:04 +0530

    fixed bug add unsigned

--- procedural_coding/change_problem/tb.sv
+++ procedural_coding/change_problem/tb.sv
@@ -4,12 +4,12 @@ module tb ;
 localparam NUM_DENOM = 4;
 localparam int DENOMINATIONS[NUM_DENOM]='{10,5,2,1}; 
 
-typedef int change_t  [NUM_DENOM] ;
+typedef int unsigned change_t  [NUM_DENOM] ;
 change_t bf_chg, recurs_chg, dp_chg;
 localparam TRIALS = 3;
 
 initial begin
-	int money;
+	int unsigned money;
 	for (int i =0; i<TRIALS;i++) begin
 		money = $urandom();
 		$display("Money %d",money);
@@ -21,23 +21,48 @@ initial begin
 end
 
 function automatic change_t  bf_change ( 
-input int money
+input int unsigned money
 );
-  int multiples[NUM_DENOM];
-$display();
+	change_t chg;
+	foreach ( DENOMINATIONS[i]) begin
+		chg[i] = money/DENOMINATIONS[i];
+		money=money%DENOMINATIONS[i];
+	end
+	return chg;
+endfunction
+
+/*
+function automatic change_t  recursive_change ( 
+input int unsigned money,
+input change_t chg
+);
+
+	change_t chg_combinations [DENOMINATIONS];
 
 	foreach ( DENOMINATIONS[i]) begin
+	  	if (money == DENOMINATIONS[0]) begin
+	  		chg[i]++;
+  			return chg;
+  		end
+  	end
+
+  	foreach ( DENOMINATIONS[i]) begin
+
 		multiples[i] = money/DENOMINATIONS[i];
 		money=money%DENOMINATIONS[i];
 	end
 	return multiples;
 endfunction
+*/
 
 function automatic void disp_change (
 input change_t chg
 );
 	foreach (chg[i])
 		$write ( "%3d x %1d \t", DENOMINATIONS[i],chg[i]);
+	$display();
+
+	$display("Total Coins %d\n",chg.sum());
 
 endfunction
 
