040bc406beb396ae3071f477a5dd1db9343ab1c7
3vm <3vm@dontemail.com>
2020-01-01 13:51:25 +0530

    port width bug

--- examples/native_parallel_bus/bus_endpoint.sv
+++ examples/native_parallel_bus/bus_endpoint.sv
@@ -12,18 +12,21 @@ input logic [DATA_WIDTH-1:0] wdata,
 output logic [DATA_WIDTH-1:0] rdata
 );
 
-logic [ADDR_WIDTH-1:0] mem [RANGE];
+localparam MEM_ADDR_WIDTH = $clog2(RANGE);
 
+logic [MEM_ADDR_WIDTH-1:0] mem [RANGE];
+logic maddr;
 logic access_here;
 
-assign access_here = (addr inside {[BASE_ADDR:+RANGE]});
+assign access_here = (maddr inside {[BASE_ADDR:+RANGE]});
+assign maddr = addr - BASE_ADDR;
 
 always @( r_wn )
 		if ( access_here && !r_wn) begin
-			mem[addr] <= wdata; //CHECK ME blocking = or non block <= for latch
+			mem[maddr] <= wdata; //CHECK ME blocking = or non block <= for latch
 			$display ( "Write to location %d data %d in instance %m" , addr, wdata);
 		end else
-			mem[addr] <= mem[addr]; //CHECK ME blocking or non blocking
+			mem[maddr] <= mem[maddr]; //CHECK ME blocking or non blocking
 
 always_comb
 	if ( r_wn == 1 && access_here ) begin
