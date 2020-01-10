8e4e6bfb2eeb62b90a5629161789644f42cf9e58
po <3vm@dontemail.com>
2020-01-10 09:33:19 +0530

    bug - clock passing even when gated

--- examples/clk_gating/tb.sv
+++ examples/clk_gating/tb.sv
@@ -7,6 +7,7 @@ bit result;
 logic rstn;
 int clkin_cnt=0, clkout_cnt=0;
 logic clken;
+bit clk_det;
 
 thee_clk_gen_module clk_gen (.clk(clkin));
 
@@ -18,9 +19,30 @@ ehgu_clkgate clkgate
 );
 
 initial begin
-  clken = 1;
   result = 1 ;
   repeat (2) @(posedge clkin);
+  
+  operate_clk_gate("ungate");
+  clock_detect (clk_det) ;
+  if ( clk_det ) 
+  	result = 1;
+  else
+  	result = 0 ;
+  
+  operate_clk_gate("gate");
+  clock_detect (clk_det) ;
+  if ( clk_det == 0 ) 
+  	result = 1;
+  else
+  	result = 0 ;
+
+  
+  operate_clk_gate("ungate");
+  clock_detect (clk_det) ;
+  if ( clk_det ) 
+  	result = 1;
+  else
+  	result = 0 ;
 
   if ( result ) begin
     repeat (3) $display ( "PASS");
@@ -30,5 +52,38 @@ initial begin
 
 	$finish;
 end
+
+task operate_clk_gate (
+input string cmd="gate"
+);
+	@(posedge clkin);
+	$display("Operating clock gate %s",cmd);
+	if ( cmd == "gate ")
+		clken = 0 ;
+	else
+		clken = 1 ;
+	repeat (2) @(posedge clkin);
+endtask
+
+task automatic clock_detect (
+output bit detected
+);
+
+int cnt_ungated, cnt_gated;
+fork
+	repeat (3) begin
+		@(posedge clkin);
+		cnt_ungated++;
+	end
+	repeat (3) begin
+		@(posedge clkout);
+		cnt_gated++;
+	end
+join_any
+$display("Clock edge count ungated %d, gated %d",cnt_ungated, cnt_gated);
+
+if ( cnt_gated > 0 ) 
+	detected = 1;
+endtask
 
 endmodule
