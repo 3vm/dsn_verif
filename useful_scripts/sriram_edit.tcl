1083901e42995f91f0bda7da23f80c480e1d8dbd
po <3vm@dontemail.com>
2020-01-30 06:11:52 +0530

    save

--- useful_scripts/sriram_edit.tcl
+++ useful_scripts/sriram_edit.tcl
@@ -14,13 +14,21 @@ set prev_line $line
 set ind_lvl 0
 
 while { [gets $fp line ] != -1 } {
-	regsub -all {===} $line { === } line
+	regsub -all {\s+} $line { } line
+	regsub -all {=} $line { = } line
+	regsub -all {=\s+=} $line {==} line
+	regsub -all {<} $line { < } line
+	regsub -all {<\s+<} $line {<<} line
+	regsub -all {>} $line { > } line
+	regsub -all {>\s+>} $line {>>} line
+	regsub -all {=\s+=\s+=} $line {===} line
 	regsub -all {!==} $line { !== } line
 	regsub -all {,} $line { , } line
 	regsub -all {:} $line { : } line
-	regsub -all {: :} $line {::} line
+	regsub -all {:\s+:} $line {::} line
 	regsub -all {\+} $line { + } line
-	regsub -all {\+ \+} $line {++} line
+	regsub -all {\+\s+\+} $line {++} line
+	regsub -all {\+\s+\=} $line {+=} line
 	regsub -all {\*\*} $line { ** } line
 
 	regsub -all {\[} $line { [ } line
@@ -31,7 +39,22 @@ while { [gets $fp line ] != -1 } {
 	regsub -all {[ ]+} $line { } line
 	regsub -all {\t} $line {  } line
 	regsub -all {\s+$} $line {} line
+	set delayed_incr_ind 0
+	if { [regexp {^\s*end\s+.*?\s+begin$} $line] } {
+		incr ind_lvl -1
+		set delayed_incr_ind 1
+	} elseif { [regexp {begin$} $line] } {
+		set delayed_incr_ind 1
+	} elseif { [regexp {^\s*end\s*} $line] } {
+		incr ind_lvl -1
+	}
+	for {set i 0} {$i<$ind_lvl} {incr i} {
+		puts -nonewline "  "
+	}
 	puts $line
+	if { $delayed_incr_ind } { 
+		incr ind_lvl 
+	}
 }
 
 close $fp
