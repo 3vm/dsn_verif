b4fcb4e5ba0500c69084eb2592fa4526dde278c7
po <3vm@dontemail.com>
2020-02-20 14:57:36 +0530

    tmp

--- examples/parallel_processing_adder/adder_parallel.sv
+++ examples/parallel_processing_adder/adder_parallel.sv
@@ -1,28 +1,51 @@
 
 module adder_parallel
-#(
-parameter WIDTH=64
-)(
-input clk,
-input logic [WIDTH-1:0] add_op0, add_op1,
-output logic [WIDTH-1:0] add_out,
-);
+# (
+parameter WIDTH = 64 ,
+parameter ENGINES = 2
+ ) (
+input logic clk ,
+input logic rstn ,
+input logic [ WIDTH-1 : 0 ] add_op0 , add_op1 ,
+output logic [ WIDTH-1 : 0 ] add_out
+ ) ;
 
-logic [WIDTH-1:0] add_op0_reg, add_op1_reg;
-logic [WIDTH-1:0] add_out_reg;
-logic [WIDTH-1:0] add_pipd_op0_reg, add_pipd_op1_reg;
-logic [WIDTH/2-1:0] add_pipd_out_reg_halfs[2];
-logic [WIDTH-1:0] add_pipd_out_reg;
-logic carry ;
+logic clkbyn ;
+logic rstn ;
 
-adder_engine 
-#(
-parameter WIDTH=64
-)(
-input clk,
-input logic [WIDTH-1:0] add_op0, add_op1,
-output logic [WIDTH-1:0] add_out,
-);
+logic [ WIDTH-1 : 0 ] add_op0_0 , add_op1_0 ;
+logic [ WIDTH-1 : 0 ] add_out_0 ;
 
+logic [ WIDTH-1 : 0 ] add_op0_1 , add_op1_1 ;
+logic [ WIDTH-1 : 0 ] add_out_1 ;
 
+counter counts upto ENGINES
+
+demux to supply every engine with unique inputs
+
+adder_engine # ( .WIDTH ( WIDTH ) ) add_0 (
+.clk ( clkbyn ) ,
+.add_op0 ( add_op0_0 ) ,
+.add_op1 ( add_op1_0 ) ,
+.add_out ( add_out_0 )
+ ) ;
+
+adder_engine # ( .WIDTH ( WIDTH ) ) add_1 (
+.clk ( clkby2 ) ,
+.add_op0 ( add_op0_1 ) ,
+.add_op1 ( add_op1_1 ) ,
+.add_out ( add_out_1 )
+ ) ;
+
+mux to combine engine outputs into correct full speed outputs
+
+ehgu_clkdiv # ( .DIVISION ( ENGINES ) ) clkdiv
+ (
+.clkin ( clk ) ,
+.rstn ,
+.en ( 1'b1 ) ,
+.clkout ( clkbyn )
+ ) ;
+ 
+logic h2h_3vm ;
 endmodule
