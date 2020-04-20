e46b90273b23ef15a5d6df4a10923eb69cba15d0
po <3vm@dontemail.com>
2020-04-20 08:17:54 +0530

    module to program, runtime reduction

--- procedural_coding/prime_factorization/tb.sv
+++ procedural_coding/prime_factorization/tb.sv
@@ -1,41 +1,40 @@
 
-module tb ;
+program tb ;
 
 initial begin
-int num,try_factor;
-int factors[$];
-num = 45;
-try_factor = 2;
-factorize(num,try_factor,factors);
-$finish;
+  int num , try_factor ;
+  int factors [ $ ] ;
+  num = 45 ;
+  try_factor = 2 ;
+  factorize ( num , try_factor , factors ) ;
+  $finish ;
 end
 
-function automatic void factorize ( 
-ref int num,
-ref int try_factor,
-ref int factors[$]
-);
+function automatic void factorize (
+ref int num ,
+ref int try_factor ,
+ref int factors [ $ ]
+ ) ;
 
-$display("number %d, trial factor %d \n Factors ", num, try_factor);
-foreach (factors[i])
-	$write ( "%d", factors[i]);
-$display();
+$display ( "number %4d , trial factor %4d \n Factors " , num , try_factor ) ;
+foreach ( factors [ i ] )
+ $write ( "%4d" , factors [ i ] ) ;
+$display ( ) ;
 
 if ( num == 1 ) begin
-	factors.push_back(1);
-	return;
+  factors.push_back ( 1 ) ;
+  return ;
 end else begin
-	if ( num % try_factor == 0 )  begin
-		num = num / try_factor ;
-		factors.push_back(try_factor);
-		$display("Factor found %d",try_factor);
-		try_factor=2;
-	end else begin
-		try_factor++;
-	end
-	factorize(num,try_factor,factors);
+  if ( num % try_factor == 0 ) begin
+    num = num / try_factor ;
+    factors.push_back ( try_factor ) ;
+    $display ( "Factor found %4d" , try_factor ) ;
+  end else begin
+    try_factor ++ ;
+  end
+  factorize ( num , try_factor , factors ) ;
 end
 
 endfunction
 
-endmodule
+endprogram
