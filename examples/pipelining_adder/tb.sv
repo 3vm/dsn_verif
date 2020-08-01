7f84bfdc515a0abfe4e56c8fa3db6426d5f44914
po <3vm@dontemail.com>
2020-08-01 07:27:03 +0530

    space added to code

--- examples/pipelining_adder/tb.sv
+++ examples/pipelining_adder/tb.sv
@@ -1,18 +1,18 @@
 
 module tb ;
 
-parameter WIDTH=64;
+parameter WIDTH = 64 ;
 
-logic clk;
-logic [WIDTH-1:0] add_nopi_op0, add_nopi_op1;
-logic [WIDTH-1:0] add_nopi_out;
-logic [WIDTH-1:0] add_pipd_op0, add_pipd_op1;
-logic [WIDTH-1:0] add_pipd_out;
-logic [WIDTH-1:0] expected;
+logic clk ;
+logic [ WIDTH-1 : 0 ] add_nopi_op0 , add_nopi_op1 ;
+logic [ WIDTH-1 : 0 ] add_nopi_out ;
+logic [ WIDTH-1 : 0 ] add_pipd_op0 , add_pipd_op1 ;
+logic [ WIDTH-1 : 0 ] add_pipd_out ;
+logic [ WIDTH-1 : 0 ] expected ;
 logic result ;
 
-adder_speedtest #(.WIDTH(WIDTH)) add_duts
-(
+adder_speedtest # ( .WIDTH ( WIDTH ) ) add_duts
+ (
 .clk ,
 
 .add_nopi_op0 ,
@@ -21,38 +21,38 @@ adder_speedtest #(.WIDTH(WIDTH)) add_duts
 
 .add_pipd_op0 ,
 .add_pipd_op1 ,
-.add_pipd_out 
-);
+.add_pipd_out
+ ) ;
 
-thee_clk_gen_module clk_gen (.clk(clk));
+thee_clk_gen_module clk_gen ( .clk ( clk ) ) ;
 
 initial begin
-result = 1;
-for ( int i = 0 ; i < 5 ; i++ ) begin
-add_nopi_op0 = $urandom() * $urandom ;
-add_nopi_op1 = $urandom() * $urandom ;
-add_pipd_op0 = add_nopi_op0 ;
-add_pipd_op1 = add_nopi_op1 ;
-expected = add_nopi_op0 + add_nopi_op1;
-repeat (4) @(posedge clk);
-if ( add_nopi_out == expected && add_pipd_out == expected ) begin
-  $display ("Vector Passed");
-end else begin
-  $display ("Vector Failed");
-  result = 0 ;
-end
-
-$display ("Test Vector Unpipelined inputs %d + %d , output %d" , add_nopi_op0, add_nopi_op1, add_nopi_out );
-$display ("Test Vector   Pipelined inputs %d + %d , output %d" , add_pipd_op0, add_pipd_op1, add_pipd_out );
-
-end
-
-if ( result ) 
-  $display ("All Vectors passed");
-else
-  $display ("Some Vectors failed");
-
-$finish;
+  result = 1 ;
+  for ( int i = 0 ; i < 5 ; i ++ ) begin
+    add_nopi_op0 = $urandom ( ) * $urandom ;
+    add_nopi_op1 = $urandom ( ) * $urandom ;
+    add_pipd_op0 = add_nopi_op0 ;
+    add_pipd_op1 = add_nopi_op1 ;
+    expected = add_nopi_op0 + add_nopi_op1 ;
+    repeat ( 4 ) @ ( posedge clk ) ;
+    if ( add_nopi_out == expected && add_pipd_out == expected ) begin
+       $display ( "Vector Passed" ) ;
+    end else begin
+       $display ( "Vector Failed" ) ;
+       result = 0 ;
+    end
+    
+    $display ( "Test Vector Unpipelined inputs %d + %d , output %d" , add_nopi_op0 , add_nopi_op1 , add_nopi_out ) ;
+    $display ( "Test Vector Pipelined inputs %d + %d , output %d" , add_pipd_op0 , add_pipd_op1 , add_pipd_out ) ;
+    
+  end
+  
+  if ( result )
+   $display ( "All Vectors passed" ) ;
+  else
+   $display ( "Some Vectors failed" ) ;
+  
+  $finish ;
 end
 
 endmodule
