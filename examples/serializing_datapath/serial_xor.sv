76b7b022973b3d60ad9fe9a6a32568583a4d0128
po <3vm@dontemail.com>
2020-08-01 07:12:37 +0530

    fix minor race condition in tb.sv, format space char change

--- examples/serializing_datapath/serial_xor.sv
+++ examples/serializing_datapath/serial_xor.sv
@@ -1,51 +1,51 @@
 
 module serial_xor (
-input logic clk,
-input logic rstn,
-input logic a,
-input logic b,
-output logic xo,
+input logic clk ,
+input logic rstn ,
+input logic a ,
+input logic b ,
+output logic xo ,
 output logic ready
-);
+ ) ;
 
-logic mem[2], memnext[2];
+logic mem [ 2 ] , memnext [ 2 ] ;
 
-typedef enum logic [2:0] {C0=0, C1=1, C2=2, C3=3, C4=4 } state_t;
+typedef enum logic [ 2 : 0 ] { C0 = 0 , C1 = 1 , C2 = 2 , C3 = 3 , C4 = 4 } state_t ;
 
-state_t state, next;
+state_t state , next ;
 
 always_comb
-	next = state.next();
+ next = state.next ( ) ;
 
 always_comb begin
-	memnext = mem;
-	ready = 0;
-	xo = 0;
-	case(state)
-		C0: memnext[0] = ~b;
-		C1: memnext[0] = mem[0] & a;
-		C2: memnext[1] = ~a;
-		C3: memnext[1] = mem[1] & b;
-		C4: begin
-				xo = mem[0] | mem[1];
-				ready = 1;
-			end
-		default: begin
-			memnext = mem;
-			ready = 0;
-			xo = 0;
-		end
-	endcase
+   memnext = mem ;
+   ready = 0 ;
+   xo = 0 ;
+   case ( state )
+   C0 : memnext [ 0 ] = ~b ;
+   C1 : memnext [ 0 ] = mem [ 0 ] & a ;
+   C2 : memnext [ 1 ] = ~a ;
+   C3 : memnext [ 1 ] = mem [ 1 ] & b ;
+   C4 : begin
+     xo = mem [ 0 ] | mem [ 1 ] ;
+     ready = 1 ;
+   end
+   default : begin
+     memnext = mem ;
+     ready = 0 ;
+     xo = 0 ;
+   end
+ endcase
 end
 
-always_ff @(posedge clk, negedge rstn)
-	if ( !rstn ) begin
-		mem <= '{default:0};
-		state <= state.first();
-	end else begin
-		mem <= memnext;
-		state <= next;
-	end
+always_ff @ ( posedge clk , negedge rstn )
+ if ( !rstn ) begin
+ mem <= '{ default : 0 } ;
+ state <= state.first ( ) ;
+ end else begin
+ mem <= memnext ;
+ state <= next ;
+ end
 
 endmodule
 
--- examples/serializing_datapath/tb.sv
+++ examples/serializing_datapath/tb.sv
@@ -1,53 +1,52 @@
 
 module tb ;
 
-logic a,b,xo,ready;
-logic clk,rstn;
-bit result;
+logic a , b , xo , ready ;
+logic clk , rstn ;
+bit result ;
 
-thee_clk_gen_module clk_gen (.clk(clk));
+thee_clk_gen_module clk_gen ( .clk ( clk ) ) ;
 
 serial_xor dut (
-.clk, 
-.rstn,
-.a, 
-.b, 
-.xo,
+.clk ,
+.rstn ,
+.a ,
+.b ,
+.xo ,
 .ready
-);
+ ) ;
 
 initial begin
-	import thee_utils_pkg::toggle_rstn;
-	toggle_rstn(.rstn(rstn));
-	a=0;b=0;
-	repeat (5) @(posedge clk);
-	result = 1;
-	@(posedge ready);
-	for(int i = 0 ; i<4;i++) begin
-		{a,b}=i;
-		while (1) begin
-			@(posedge clk);
-			if ( ready) begin
-				$display("Inputs a %b b %b output a^b %b",a,b,xo);
-				if ( xo !== a^b )
-					result = 0;
-				break;
-			end
-		end
-	end
-
-	if ( result ) 
-  		$display ("All Vectors passed");
-	else
-		$display ("Some Vectors failed");
-
-	$finish;
+   import thee_utils_pkg :: toggle_rstn ;
+   toggle_rstn ( .rstn ( rstn ) ) ;
+   a = 0 ; b = 0 ;
+   repeat ( 5 ) @ ( posedge clk ) ;
+   result = 1 ;
+   @ ( posedge ready ) ;
+   @ (posedge clk);
+   for ( int i = 0 ; i < 4 ; i ++ ) begin
+     { a , b } = i ;
+     $display("Inputs applied a=%b b=%b",a,b);
+     while ( 1 ) begin
+       @ ( posedge clk ) ;
+       $display ( "State of scheduler %s , inputs %b , %b output xor %b , memory %b , %b" , dut.state.name ( ) , a , b , xo , dut.mem [ 0 ] , dut.mem [ 1 ] ) ;
+       if ( ready ) begin
+         $display ( "Result ready for inputs a %b b %b output a^b %b" , a , b , xo ) ;
+         $display ("------------------------------------------------------");
+         if ( xo !== a^b )
+           result = 0 ;
+         break ;
+       end
+     end
+   end
+  
+   if ( result )
+     $display ( "All Vectors passed" ) ;
+   else
+     $display ( "Some Vectors failed" ) ;
+  
+   $finish ;
 end
-
-initial
-	forever @(posedge clk)
-		$display("State of scheduler %s, inputs %b,%b output xor %b, memory %b , %b", dut.state.name(), a,b,xo,dut.mem[0],dut.mem[1]);
-
 
 endmodule
 
