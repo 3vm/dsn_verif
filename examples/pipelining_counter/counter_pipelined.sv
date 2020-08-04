32f4c1a9f8e6dfc9a2d5c4558a16eabf80571984
po <3vm@dontemail.com>
2020-08-04 11:45:16 +0530

    adding space

--- examples/pipelining_counter/counter_pipelined.sv
+++ examples/pipelining_counter/counter_pipelined.sv
@@ -1,26 +1,26 @@
 
 module counter_pipelined
-#(
-parameter WIDTH=64
-)(
-input logic clk,
-input logic rstn,
-output logic [WIDTH-1:0] cnt_pipd_out
-);
+# (
+parameter WIDTH = 64
+ ) (
+input logic clk ,
+input logic rstn ,
+output logic [ WIDTH-1 : 0 ] cnt_pipd_out
+ ) ;
 
 logic future_carry ;
 
-always @(posedge clk) begin
-	if (!rstn) begin
-		future_carry <= 0 ;
-		cnt_pipd_out[WIDTH/2-1:0] <= 0 ;
-		cnt_pipd_out[WIDTH-1:WIDTH/2] <= 0 ;
-	end else begin
-		future_carry <= (cnt_pipd_out[WIDTH/2-1:0] == ({(WIDTH/2) {1'b1}} - 1'b1 ) );
-		$display ("future carry %d cnt %b",future_carry, cnt_pipd_out);
-		cnt_pipd_out[WIDTH/2-1:0] <= cnt_pipd_out[WIDTH/2-1:0] + 1;
-		cnt_pipd_out[WIDTH-1:WIDTH/2] <= cnt_pipd_out[WIDTH-1:WIDTH/2]+future_carry;
-	end
+always @ ( posedge clk ) begin
+   if ( !rstn ) begin
+     future_carry <= 0 ;
+     cnt_pipd_out [ ( WIDTH / 2 ) - 1 : 0 ] <= 0 ;
+     cnt_pipd_out [ WIDTH-1 : WIDTH / 2 ] <= 0 ;
+   end else begin
+     future_carry <= ( cnt_pipd_out [ ( WIDTH / 2 ) - 1 : 0 ] == ( { ( WIDTH / 2 ) { 1'b1 } } - 1'b1 ) ) ;
+     $display ( "future carry %d cnt %b" , future_carry , cnt_pipd_out ) ;
+     cnt_pipd_out [ ( WIDTH / 2 ) - 1 : 0 ] <= cnt_pipd_out [ ( WIDTH / 2 ) - 1 : 0 ] + 1 ;
+     cnt_pipd_out [ WIDTH-1 : WIDTH / 2 ] <= cnt_pipd_out [ WIDTH-1 : WIDTH / 2 ] + future_carry ;
+   end
 end
 
 endmodule
--- examples/pipelining_counter/counter_speedtest.sv
+++ examples/pipelining_counter/counter_speedtest.sv
@@ -1,36 +1,36 @@
 
 module counter_speedtest
-#(
-parameter WIDTH=64
-)(
-input logic clk,
-input logic rstn,
-output logic [WIDTH-1:0] cnt_nopi_out,
-output logic [WIDTH-1:0] cnt_pipd_out
-);
+# (
+parameter WIDTH = 64
+ ) (
+input logic clk ,
+input logic rstn ,
+output logic [ WIDTH-1 : 0 ] cnt_nopi_out ,
+output logic [ WIDTH-1 : 0 ] cnt_pipd_out
+ ) ;
 
-logic [WIDTH/2-1:0] cnt_pipd_out_halfs[2];
+logic [ ( WIDTH / 2 ) - 1 : 0 ] cnt_pipd_out_halfs [ 2 ] ;
 logic carry ;
 
-always @(posedge clk, negedge rstn) begin
-	if (!rstn) 
-		cnt_nopi_out <= 0 ;
-	else		
-		cnt_nopi_out <= cnt_nopi_out + 1 ;
+always @ ( posedge clk , negedge rstn ) begin
+   if ( !rstn )
+   cnt_nopi_out <= 0 ;
+   else
+   cnt_nopi_out <= cnt_nopi_out + 1 ;
 end
 
-always @(posedge clk) begin
-	if (!rstn) begin
-		{carry, cnt_pipd_out_halfs[0]} <= 0 ;
-		cnt_pipd_out_halfs[1] <= 0 ;
-		cnt_pipd_out[WIDTH/2-1:0] <= 0 ;
-		cnt_pipd_out[WIDTH-1:WIDTH/2] <= 0 ;
-	end else begin
-		{carry, cnt_pipd_out_halfs[0]} <= cnt_pipd_out[WIDTH/2-1:0] + 1 ;
-		cnt_pipd_out_halfs[1] <= cnt_pipd_out[WIDTH-1 : WIDTH/2] ;
-		cnt_pipd_out[WIDTH/2-1:0] <= cnt_pipd_out_halfs[0];
-		cnt_pipd_out[WIDTH-1:WIDTH/2] <= cnt_pipd_out_halfs[1]+carry;
-	end
+always @ ( posedge clk ) begin
+   if ( !rstn ) begin
+     { carry , cnt_pipd_out_halfs [ 0 ] } <= 0 ;
+     cnt_pipd_out_halfs [ 1 ] <= 0 ;
+     cnt_pipd_out [ ( WIDTH / 2 ) - 1 : 0 ] <= 0 ;
+     cnt_pipd_out [ WIDTH-1 : WIDTH / 2 ] <= 0 ;
+   end else begin
+     { carry , cnt_pipd_out_halfs [ 0 ] } <= cnt_pipd_out [ ( WIDTH / 2 ) - 1 : 0 ] + 1 ;
+     cnt_pipd_out_halfs [ 1 ] <= cnt_pipd_out [ WIDTH-1 : WIDTH / 2 ] ;
+     cnt_pipd_out [ ( WIDTH / 2 ) - 1 : 0 ] <= cnt_pipd_out_halfs [ 0 ] ;
+     cnt_pipd_out [ WIDTH-1 : WIDTH / 2 ] <= cnt_pipd_out_halfs [ 1 ] + carry ;
+   end
 end
 
 endmodule
