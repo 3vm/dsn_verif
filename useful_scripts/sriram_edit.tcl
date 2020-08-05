9ccf7c5e352c4cf5e44c0304ad22801ee62404dc
po <3vm@dontemail.com>
2020-08-05 08:44:07 +0530

    added multiple file editing feature

--- useful_scripts/sriram_edit.tcl
+++ useful_scripts/sriram_edit.tcl
@@ -2,85 +2,96 @@
 #editing format in honor of Software Engineer Ram
 #
 
-set filename [ lindex $argv 0 ]
-set tempoutfile ".tmp_z13asxr93eqwe523"
+proc format_one_file { fl } {
 
-puts "Formatting $filename"
+	puts "Formatting $fl"
+	set tempoutfile ".tmp_z13asxr93eqwe523"
 
-set fp [ open $filename r]
+	set fp [ open $fl r]
 
-set fpo [ open $tempoutfile w ]
+	set fpo [ open $tempoutfile w ]
 
-set line {}
-set prev_line $line
+	set line {}
+	set prev_line $line
 
-set ind_lvl 0
+	set ind_lvl 0
 
-while { [gets $fp line ] != -1 } {
-	regsub -all {\s+} $line { } line
-	regsub -all {=} $line { = } line
-	regsub -all {>} $line { > } line
-	regsub -all {=\s*=\s*=} $line {===} line
-	regsub -all {=\s*=} $line {==} line
-	regsub -all {!\s*=} $line {!=} line
-	regsub -all {~\s*=} $line {~=} line
-	regsub -all {\^\s*=} $line {^=} line
-	regsub -all {<} $line { < } line
-	regsub -all {<\s+<} $line {<<} line
-	regsub -all {<\s*=} $line {<=} line
-	regsub -all {>\s*=} $line {>=} line
-	regsub -all {>\s+>} $line {>>} line
-	regsub -all {!==} $line { !== } line
-	regsub -all {,} $line { , } line
-	regsub -all {:} $line { : } line
-	regsub -all {:\s+:} $line {::} line
-	regsub -all {\+} $line { + } line
-	regsub -all {\+\s+\+} $line {++} line
-	regsub -all {\+\s+\=} $line {+=} line
+	while { [gets $fp line ] != -1 } {
+		regsub -all {\s+} $line { } line
+		regsub -all {=} $line { = } line
+		regsub -all {>} $line { > } line
+		regsub -all {=\s*=\s*=} $line {===} line
+		regsub -all {=\s*=} $line {==} line
+		regsub -all {!\s*=} $line {!=} line
+		regsub -all {~\s*=} $line {~=} line
+		regsub -all {\^\s*=} $line {^=} line
+		regsub -all {<} $line { < } line
+		regsub -all {<\s+<} $line {<<} line
+		regsub -all {<\s*=} $line {<=} line
+		regsub -all {>\s*=} $line {>=} line
+		regsub -all {>\s+>} $line {>>} line
+		regsub -all {!==} $line { !== } line
+		regsub -all {,} $line { , } line
+		regsub -all {:} $line { : } line
+		regsub -all {:\s+:} $line {::} line
+		regsub -all {\+} $line { + } line
+		regsub -all {\+\s+\+} $line {++} line
+		regsub -all {\+\s+\=} $line {+=} line
 
-	regsub -all {\*} $line { * } line
-	regsub -all {\*\s*\*} $line {**} line
-	regsub -all {\*\s*\=} $line {*=} line
-	regsub -all {&\s*=} $line {\&=} line
+		regsub -all {\*} $line { * } line
+		regsub -all {\*\s*\*} $line {**} line
+		regsub -all {\*\s*\=} $line {*=} line
+		regsub -all {&\s*=} $line {\&=} line
 
-	regsub -all {/} $line { / } line
-	regsub -all {/\s*/} $line {//} line
-	regsub -all {\/\s*\*} $line {/*} line
-	regsub -all {\*\s*\/} $line {*/} line
-	regsub -all {/\s*=} $line {/=} line
-	regsub -all {/\s*/} $line { // } line
+		regsub -all {/} $line { / } line
+		regsub -all {/\s*/} $line {//} line
+		regsub -all {\/\s*\*} $line {/*} line
+		regsub -all {\*\s*\/} $line {*/} line
+		regsub -all {/\s*=} $line {/=} line
+		regsub -all {/\s*/} $line { // } line
 
-	regsub -all {\[} $line { [ } line
-	regsub -all {\]} $line { ] } line
-	regsub -all {\(} $line { ( } line
-	regsub -all {\)} $line { ) } line
-	regsub -all {\{} $line "\{ " line
-	regsub -all {\}} $line " \}" line
-	regsub -all {;} $line { ; } line
-	regsub -all {[ ]+} $line { } line
-	regsub -all {\t} $line {  } line
-	regsub -all {\s+$} $line {} line
-	set delayed_incr_ind 0
-	if { [regexp {^\s*end\s+.*?\s+begin$} $line] } {
-		incr ind_lvl -1
-		set delayed_incr_ind 1
-	} elseif { [regexp {begin$} $line] } {
-		set delayed_incr_ind 1
-	} elseif { [regexp {^\s*end\s*} $line] } {
-		incr ind_lvl -1
+		regsub -all {\[} $line { [ } line
+		regsub -all {\]} $line { ] } line
+		regsub -all {\(} $line { ( } line
+		regsub -all {\)} $line { ) } line
+		regsub -all {\{} $line "\{ " line
+		regsub -all {\}} $line " \}" line
+		regsub -all {;} $line { ; } line
+		regsub -all {[ ]+} $line { } line
+		regsub -all {\t} $line {  } line
+		regsub -all {\s+$} $line {} line
+		set delayed_incr_ind 0
+		if { [regexp {^\s*end\s+.*?\s+begin$} $line] } {
+			incr ind_lvl -1
+			set delayed_incr_ind 1
+		} elseif { [regexp {begin$} $line] } {
+			set delayed_incr_ind 1
+		} elseif { [regexp {^\s*end\s*} $line] } {
+			incr ind_lvl -1
+		}
+		for {set i 0} {$i<$ind_lvl} {incr i} {
+			puts -nonewline $fpo "  "
+		}
+		puts $fpo $line
+		if { $delayed_incr_ind } { 
+			incr ind_lvl 
+		}
 	}
-	for {set i 0} {$i<$ind_lvl} {incr i} {
-		puts -nonewline $fpo "  "
-	}
-	puts $fpo $line
-	if { $delayed_incr_ind } { 
-		incr ind_lvl 
-	}
+
+	close $fp
+	close $fpo
+
+	file rename -force $tempoutfile $fl
 }
 
-close $fp
-close $fpo
+set files [ lindex $argv 0 ]
 
-file rename -force $tempoutfile $filename
+if { $files == "" } {
+  foreach fl [ glob *.sv ] {
+    format_one_file $fl
+  }
+} else {
+  format_one_file $files
+}
 
 #3vm, start Jan 2020
