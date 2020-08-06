990674c42f3155988e15be8dedd7d8f3b5f106da
po <3vm@dontemail.com>
2020-08-06 18:15:38 +0530

    fixed int to real cast, range check for error on output

--- examples/delta_sigma_adc_model/tb.sv
+++ examples/delta_sigma_adc_model/tb.sv
@@ -19,7 +19,7 @@ parameter real FREQ_CLK_OVERSAMP=256;
 
 thee_clk_gen_module # ( .FREQ ( FREQ_CLK_OVERSAMP / OVERSAMP_RATIO ) ) clk_gen ( .clk ( clk ) ) ;
 thee_clk_gen_module # ( .FREQ ( FREQ_CLK_OVERSAMP ) )clk_gen_oversamp ( .clk ( clk_oversamp ) ) ;
-assign dig_out_real = dig_out / OVERSAMP_RATIO ;
+assign dig_out_real = dig_out / (OVERSAMP_RATIO/2.0) ;
 
 ds_adc # ( .WIDTH ( WIDTH ), .OVERSAMP_RATIO(OVERSAMP_RATIO) ) ds_adc
  (
@@ -38,7 +38,7 @@ initial begin
    repeat ( 10 ) @ ( posedge clk ) ;
    
    for ( int i = 0 ; i < 5 ; i ++ ) begin
-     ana_in = urand_range_real ( 0 , 1.0 ) ;
+     ana_in = urand_range_real ( 0 , 2.0 ) - 1.0;
      repeat (10) @ ( posedge clk ) ;
      check_result ;
   end
@@ -49,7 +49,7 @@ end
 task check_result ;
  import thee_utils_pkg :: compare_real_fixed_err ;
  $display ( "Analog input %f , Digital output %d , Output reconverted to analog %f" , ana_in , dig_out , dig_out_real ) ;
- compare_real_fixed_err ( .expected ( ana_in ) ,  .actual ( dig_out_real ) , .result ( result ) , .max_err ( 1.001 * 1.0 / 256  ) ) ;
+ compare_real_fixed_err ( .expected ( ana_in ) ,  .actual ( dig_out_real ) , .result ( result ) , .max_err ( 1.001 * 1.0 * 2 / 256  ) ) ;
  if ( result )
  $display ( "PASS" ) ;
  else begin
