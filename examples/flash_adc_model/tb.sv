9f670197fc0f7af88c7352cb4ff5f1cb6f4f5369
po <3vm@dontemail.com>
2020-08-06 16:04:26 +0530

    fixed bug in comparing real number values - need fixed error compare

--- examples/flash_adc_model/tb.sv
+++ examples/flash_adc_model/tb.sv
@@ -17,27 +17,20 @@ fadc dut
  ) ;
 
 initial begin
+   import thee_utils_pkg :: urand_range_real ;
+   for ( int i = 0 ; i < 20 ; i ++ ) begin
+     ana_in = urand_range_real ( 0, 1.0);
    #1 ;
-   ana_in = 0.6 ;
-   check_result ;
-   #1 ;
-   ana_in = 0.3 ;
-   check_result ;
-   #1 ;
-   ana_in = 0.25 ;
-   check_result ;
-   #1 ;
-   ana_in = 0.81 ;
-   check_result ;
-   #1 ;
+	 check_result;
+   end
    $finish ;
 end
 
-task check_result ;
- import thee_utils_pkg :: check_approx_equality ;
- #0 ;
+task automatic check_result ;
+ import thee_utils_pkg :: compare_real_fixed_err ;
  $display ( "Analog input %f , Digital output %d , Output reconverted to analog %f" , ana_in , dig_out , dig_out_real ) ;
- check_approx_equality ( .inp ( dig_out_real ) , .expected ( ana_in ) , .result ( result ) , .tolerance ( 100 * 1.001 * 1.0 / 256 ) ) ;
+ compare_real_fixed_err ( .expected ( ana_in ) , .actual ( dig_out_real ) , .result ( result ) , .max_err ( 1.001 * 1.0 / 256 ) ) ;
+
  if ( result )
  $display ( "PASS" ) ;
  else begin
