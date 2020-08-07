01d0f59362ed7a01c44515b47b030a366e7d982e
po <3vm@dontemail.com>
2020-08-07 08:46:19 +0530

    formatted code

--- examples/cdr_model/cdr_model.sv
+++ examples/cdr_model/cdr_model.sv
@@ -1,63 +1,63 @@
-module cdr_model 
-(
-input logic data_in,
-output logic clkout,
+module cdr_model
+ (
+input logic data_in ,
+output logic clkout ,
 output logic lock
-);
+ ) ;
 
-timeunit 1ns;
-timeprecision 1ps;
+timeunit 1ns ;
+timeprecision 1ps ;
 
-localparam CDR_LOCKING_WINDOW=20;
-localparam realtime NATIVE_CLOCK_PERIOD=40;
+localparam CDR_LOCKING_WINDOW = 20 ;
+localparam realtime NATIVE_CLOCK_PERIOD = 40 ;
 
-realtime period_avg, this_period;
-realtime curr_edge, prev_edge,min_period;
+realtime period_avg , this_period ;
+realtime curr_edge , prev_edge , min_period ;
 
 initial begin
-	repeat (CDR_LOCKING_WINDOW) @(data_in);
-	lock = 1;
+   repeat ( CDR_LOCKING_WINDOW ) @ ( data_in ) ;
+   lock = 1 ;
 end
 
 initial begin
-	prev_edge = $realtime();
-	min_period=NATIVE_CLOCK_PERIOD;
-	forever @(data_in) begin
-		curr_edge = $realtime();
-		this_period = curr_edge - prev_edge;
-		prev_edge = curr_edge;
-		if ( min_period > this_period ) begin
-			$display ("Shrink: Min period %1.5e" , min_period);
-			$display ("Shrink: This period %1.5e" , this_period);
-			min_period = 0.5 * this_period + 0.5 * min_period;
-		end	else if ( min_period > 0.95 * this_period  && min_period < 1.05 * this_period ) begin
-			$display ("Expand: Min period %1.5e" , min_period);
-			$display ("Expand: This period %1.5e" , this_period);
-			min_period = 0.5 * this_period + 0.5 * min_period;
-		end
-	end
+   prev_edge = $realtime ( ) ;
+   min_period = NATIVE_CLOCK_PERIOD ;
+   forever @ ( data_in ) begin
+     curr_edge = $realtime ( ) ;
+     this_period = curr_edge - prev_edge ;
+     prev_edge = curr_edge ;
+     if ( min_period > this_period ) begin
+       $display ( "Shrink : Min period %1.5e" , min_period ) ;
+       $display ( "Shrink : This period %1.5e" , this_period ) ;
+       min_period = 0.5 * this_period + 0.5 * min_period ;
+     end else if ( min_period > 0.95 * this_period && min_period < 1.05 * this_period ) begin
+       $display ( "Expand : Min period %1.5e" , min_period ) ;
+       $display ( "Expand : This period %1.5e" , this_period ) ;
+       min_period = 0.5 * this_period + 0.5 * min_period ;
+     end
+   end
 end
 
 initial begin
-	clkout = 0 ;
-	forever begin
-	    fork 
-	    	begin
-				#(min_period/2.0);
-				clkout= 1;
-				#(min_period/2.0);
-				clkout= 0;
-			end
-			begin
-				@(data_in);
-			end
-		join_any
-		disable fork;
-		clkout=0;
-	end
+   clkout = 0 ;
+   forever begin
+     fork
+     begin
+       # ( min_period / 2.0 ) ;
+       clkout = 1 ;
+       # ( min_period / 2.0 ) ;
+       clkout = 0 ;
+     end
+     begin
+       @ ( data_in ) ;
+     end
+     join_any
+     disable fork ;
+     clkout = 0 ;
+   end
 end
 
-initial 
-	$monitor("Clock %b , data %b , time %t", clkout,data_in, $realtime());
+initial
+ $monitor ( "Clock %b , data %b , time %t" , clkout , data_in , $realtime ( ) ) ;
 
 endmodule
--- examples/cdr_model/tb.sv
+++ examples/cdr_model/tb.sv
@@ -1,56 +1,56 @@
 
 module tb ;
-import thee_utils_pkg::check_approx_equality;
-import thee_utils_pkg::urand_range_real;
+import thee_utils_pkg :: check_approx_equality ;
+import thee_utils_pkg :: urand_range_real ;
 
-localparam real REF_FREQ = 100e6;
-logic clk_ref,clk_vco;
+localparam real REF_FREQ = 100e6 ;
+logic clk_ref , clk_vco ;
 
-real fout0,exp_fout;
-bit result0,result1;
-logic rstn;
-logic pll_lock;
-logic data_in;
+real fout0 , exp_fout ;
+bit result0 , result1 ;
+logic rstn ;
+logic pll_lock ;
+logic data_in ;
 
-thee_clk_gen_module #(.FREQ(REF_FREQ/1e6),.CLK_GEN_TYPE("jitter_only"),.PP_JITTER_PPM(10000)) ref_gen (.clk(clk_ref));
+thee_clk_gen_module # ( .FREQ ( REF_FREQ / 1e6 ) , .CLK_GEN_TYPE ( "jitter_only" ) , .PP_JITTER_PPM ( 10000 ) ) ref_gen ( .clk ( clk_ref ) ) ;
 
 initial begin
-  data_in = 0 ;
-  forever begin
-    repeat (1) @(posedge clk_ref);
-      data_in = 1;
-    repeat (3) @(posedge clk_ref);
-      data_in = 0 ;
-  end
+   data_in = 0 ;
+   forever begin
+     repeat ( 1 ) @ ( posedge clk_ref ) ;
+     data_in = 1 ;
+     repeat ( 3 ) @ ( posedge clk_ref ) ;
+     data_in = 0 ;
+   end
 end
 
 cdr_model cdr
-(
- .data_in,
- .clkout (clk_vco),
- .lock (pll_lock)
-);
+ (
+ .data_in ,
+ .clkout ( clk_vco ) ,
+ .lock ( pll_lock )
+ ) ;
 
-thee_clk_freq_meter #(.MEAS_WINDOW(50)) fmeter0  (.clk(clk_vco),.freq_in_hertz(fout0));
+thee_clk_freq_meter # ( .MEAS_WINDOW ( 50 ) ) fmeter0 ( .clk ( clk_vco ) , .freq_in_hertz ( fout0 ) ) ;
 
 initial begin
-  repeat (2) @(posedge clk_ref);
-  rstn=0;
-  repeat (10) @(posedge clk_ref);
-  rstn=1;  
-
-  repeat (500) @(posedge clk_vco);
-
-  exp_fout = REF_FREQ ;
-  $display ( " Clkout frequencies %1.3e , expected %1.3e", fout0, exp_fout);
-  check_approx_equality (.inp(fout0),.expected(exp_fout),.result(result0));
-  if ( result0==1) begin
-    repeat (3) $display ( "PASS");
-  end else begin
-    repeat (3) $display ( "FAIL");
-  end 
-
-	$finish;
+   repeat ( 2 ) @ ( posedge clk_ref ) ;
+   rstn = 0 ;
+   repeat ( 10 ) @ ( posedge clk_ref ) ;
+   rstn = 1 ;
+  
+   repeat ( 500 ) @ ( posedge clk_vco ) ;
+  
+   exp_fout = REF_FREQ ;
+   $display ( " Clkout frequencies %1.3e , expected %1.3e" , fout0 , exp_fout ) ;
+   check_approx_equality ( .inp ( fout0 ) , .expected ( exp_fout ) , .result ( result0 ) ) ;
+   if ( result0 == 1 ) begin
+     repeat ( 3 ) $display ( "PASS" ) ;
+   end else begin
+     repeat ( 3 ) $display ( "FAIL" ) ;
+   end
+  
+   $finish ;
 end
 
 endmodule
