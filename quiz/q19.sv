5461553b53c80d011edbe42306433e886c1f1232
po <3vm@dontemail.com>
2020-08-21 11:46:58 +0530

    Bug - unknown syntax and sematic error

---
+++ quiz/q19.sv
@@ -0,0 +1,21 @@
+program tb;
+
+class automatic t;
+  rand int unsigned i;
+  constraint mycon { myfn() == 0 && i > 1 && i < 16 ; };
+  function int myfn ();
+    for ( int j=2;j<i;j++) begin
+      if (i %j == 0) 
+      	return -1;
+      return 0;
+  end
+  endfunction
+endclass
+
+initial begin
+   t var1;
+   var1.randomize(void);
+   repeat (10) $display("Randomization result i = %d",var1.i);
+end
+
+endprogram
