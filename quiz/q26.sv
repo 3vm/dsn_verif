60905bc8a2ce4247c40c57a1bf157ae13494346a
po <3vm@dontemail.com>
2020-08-28 12:16:55 +0530

    more constrained random questions

---
+++ quiz/q26.sv
@@ -0,0 +1,7 @@
+module mym;
+  initial $display("Printing from %m");
+endmodule
+
+module tb;
+  mym i[0:1] ();
+endmodule
---
+++ quiz/q27.sv
@@ -0,0 +1,25 @@
+program tb;
+
+class myc;
+  rand int k;
+  rand bit [2:0] m;
+  constraint con1 { 
+    k inside {[3:8]} ;
+    k dist {3:=1,4:=1,5:=2,6:=2,7:=3,8:=3};
+  }
+endclass
+
+initial begin
+  int cnt, cnt2;
+  repeat (100000) begin
+    automatic myc c=new();
+    if ( c.randomize()==1) begin
+      //$write("%3d ",c.k);
+      cnt++;
+      if ( c.k==5) cnt2++;
+    end
+  end
+  $display("Frequency of 5 = %f",1.0*cnt2/cnt);
+end
+
+endprogram
