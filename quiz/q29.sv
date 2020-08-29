149933baabbcf712b2fdbebed030f22f658e39e2
po <3vm@dontemail.com>
2020-08-29 07:46:25 +0530

    fixed - use or condition

--- quiz/q29.sv
+++ quiz/q29.sv
@@ -5,7 +5,7 @@ initial begin
   repeat (60*10) begin
     do 
       i = $urandom_range(60);
-    while ( i==10 && i==20 && i%7==0 );
+    while ( i==10 || i==20 || i%7==0 );
     if ( i == 56 ) begin
   	  $display("\nWrong condition %d",i);
   	  $finish;
