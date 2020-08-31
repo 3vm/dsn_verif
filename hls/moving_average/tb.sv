c4320a239d23c5b4768722d23a5dcc301fb98735
po <3vm@dontemail.com>
2020-08-31 18:33:35 +0530

    moving average passes for const input

--- hls/moving_average/tb.sv
+++ hls/moving_average/tb.sv
@@ -12,31 +12,32 @@ logic [ DWIDTH-1 : 0 ] data_out , expected_data ;
 logic result ;
 logic [ DWIDTH-1 : 0 ] filter [ $ ] ;
 logic start, done, idle, ready;
+int cnt=0;
 
 thee_clk_gen_module #(.FREQ(100)) clk_gen_i0 ( .clk ( clk ) ) ;
 
 initial begin
   repeat (TAPS) filter.push_front(data_in);
-  result = 1 ;   data_in=100;
+  result = 1 ;
   toggle_rstn ( .rstn ( rstn ) ) ; 
-  $display("Start %b, ready %b, idle %b, done %b", start, ready, idle, done);  
   start = 1 ;
   wait(ready);
-  $display("Start %b, ready %b, idle %b, done %b", start, ready, idle, done);  
-  data_in=100;
-  repeat(10) @(posedge clk);
-  $display("Start %b, ready %b, idle %b, done %b", start, ready, idle, done);  
   $display("MA filter ready");
 
-  for ( int i = 0 ; i < 2*TAPS ; i ++ ) begin
+  for ( int i = 0 ; i < 40*TAPS ; i ++ ) begin
     repeat ( 1 ) @ ( posedge clk ) ;
     if ( ready ) begin
       data_in = 100;//$urandom ( ) ;
       filter.push_front(data_in); filter.pop_back();
     end 
     if ( done ) begin
+      if ( cnt < TAPS ) begin
+        cnt++;
+        continue;
+      end
       $display("Start %b, ready %b, idle %b, done %b", start, ready, idle, done);
       expected_data = filter.sum()/TAPS;
+      cnt++;
       if ( data_out === expected_data ) begin
         $display ( "P - output data %d expected data %d" , data_out , expected_data ) ;
       end else begin
