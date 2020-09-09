cefffa021ea91cb6318b011b855434800962c41e
po <3vm@dontemail.com>
2020-09-09 06:51:23 +0530

    unique not supported for simulation in Vivado yet

--- examples/lfsr/tb.sv
+++ examples/lfsr/tb.sv
@@ -3,21 +3,29 @@ program tb ;
 parameter WIDTH = 4 ;
 
 logic result ;
-logic [ WIDTH-1 : 0 ] lfsr, polynomial ;
-logic [ WIDTH-1 : 0 ] lfsr_seq [$ ] ;
+logic [ WIDTH-1 : 0 ] lfsr1,lfsr2,lfsr3, polynomial1,polynomial2,polynomial3 ;
+typedef logic [ WIDTH-1 : 0 ] lfsr_seq_t [$ ] ;
+lfsr_seq_t sq1,sq2,sq3;
 
 import ehgu_basic_pkg::lfsr_logic;
 
 initial begin
    result = 1 ;
-   polynomial = 'h3;//'h C ;
-   lfsr=1;
+   polynomial1 = 'h3;//'h C ;
+   polynomial2 = 'h9;//'h 9 ;
+   polynomial3 = 'h5;//'h A ;
+   lfsr1=1; lfsr2 = 1 ; lfsr3 = 1;
 
    for ( int i = 0 ; i < 20 ; i ++ ) begin
-     lfsr = lfsr_logic ( .lfsr_reg ( lfsr ) , .polynomial ( polynomial ), .lfsr_width(WIDTH) ) ;
-      #0 ;
-     $display ( "SNo.%3d LFSR %b " , i, lfsr ) ;
+     lfsr1 = lfsr_logic ( .lfsr_reg ( lfsr1 ) , .polynomial ( polynomial1 ), .lfsr_width(WIDTH) ) ;
+     lfsr2 = lfsr_logic ( .lfsr_reg ( lfsr2 ) , .polynomial ( polynomial2 ), .lfsr_width(WIDTH) ) ;
+     lfsr3 = lfsr_logic ( .lfsr_reg ( lfsr3 ) , .polynomial ( polynomial3 ), .lfsr_width(WIDTH) ) ;
+     $display ( "SNo.%3d LFSR %b LFSR %b LFSR %b" , i, lfsr1,lfsr2,lfsr3 ) ;
+     sq1[i]=lfsr1 ; sq2[i]=lfsr2 ; sq3[i]=lfsr3 ; 
    end
+
+   sq1 = sq1.unique(); 
+   $display("Length of sequence 1 %d",sq1.size());
 
   if ( result )
    $display ( "All Vectors passed" ) ;
