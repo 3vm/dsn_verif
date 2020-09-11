16cdfec0238f88ab940bd232f2cdd0e8e264e980
po <3vm@dontemail.com>
2020-09-11 16:16:22 +0530

    assertion quiz question, possibly not supported by Vivado

---
+++ quiz/q43.sv
@@ -0,0 +1,37 @@
+`timescale 1ns/100ps
+module tb;
+  //which assertion never fails? 
+  //ans: i don't know . Vivado is possibly not supporting all syntax of assertions
+
+bit [9:0] km='b0000111000,kn='b000001100;
+bit m,n,clk;
+
+always clk = #1 ~clk;
+
+initial begin
+  int cnt;
+  repeat (10) @(posedge clk) begin
+    m = km[cnt]; n=kn[cnt++];
+    $display("m %b, n %b,count %d",m,n,cnt);
+  end
+  $finish;
+end
+
+sequence pulse_m;
+  @(posedge clk) ~m ##  m  ##[1:3] ~m;
+endsequence
+
+sequence pulse_n;
+  @(posedge clk) ~n ##  n ## [1:3] ~n;
+endsequence
+
+sequence s1;
+  @(posedge clk) pulse_m intersect pulse_n;
+endsequence
+
+a1: assert property (@(posedge clk) s1); 
+a2: assert property (@(posedge clk) m ##1 n); 
+a3: assert property (@(posedge clk) m ## [0:4] n); 
+a4: assert property (@(posedge clk) m ## [0:$] n); 
+
+endmodule
