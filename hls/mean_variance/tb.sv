80da01dac84e2ca3b36eeb73665e41882547a858
po <3vm@dontemail.com>
2020-09-14 18:28:31 +0530

    bug - variance formula wrong in C, eyeballing pass for c vs verilog, failing automation

--- hls/mean_variance/tb.sv
+++ hls/mean_variance/tb.sv
@@ -3,21 +3,20 @@ module tb ;
 import thee_utils_pkg :: * ;
 
 parameter DWIDTH = 32 ;
-parameter TAPS = 2;
-parameter IGNORE_FIRST_N = 2;
+parameter TAPS = 4;
 
 bit clk ;
 logic rstn ;
-logic [ DWIDTH-1 : 0 ] data_in ;
-logic [ DWIDTH-1 : 0 ] data_out , expected_data, expected_data_d;
-logic [ DWIDTH-1 : 0 ] filter [ $ ] ;
+logic [ DWIDTH-1 : 0 ] data_in,data_in_dly ;
+logic [ DWIDTH-1 : 0 ] data_out , expected_data;
+logic [ DWIDTH-1 : 0 ] data_buf [TAPS];
 logic start, done, idle, ready;
 int cnt=0;
 int data_cnt=0;
 logic result;
 
 logic  [1:0] data_in_address0;
-logic   data_in_ce0;
+logic   data_in_ce0, data_in_ce0_d;
 logic  [31:0] data_in_q0;
 logic  [0:0] data_out_address0;
 logic   data_out_ce0;
@@ -30,27 +29,37 @@ logic  [31:0] data_out_d1;
 
 thee_clk_gen_module #(.FREQ(100)) clk_gen_i0 ( .clk ( clk ) ) ;
 bit first_cycle;
+
 initial begin
-  repeat (TAPS) filter.push_front(data_in);
+  data_in=0;
   forever @(posedge clk )
-    if (data_cnt< 1) begin
+    if ( data_in_ce0 )
+       data_in = $urandom_range(100) ;
+end
+
+initial begin
+  data_in=0; data_in_ce0_d= 0 ;
+  forever @(posedge clk ) begin
+    data_in_ce0_d = data_in_ce0; 
+    if (data_cnt< 4) begin
       start =  1;
       if ( !ready && !idle ) begin
-        data_in = $urandom_range(200) ;
-        filter.push_front(data_in); filter.pop_back();
-        //$display("Data %d, Data number %d",data_in, data_cnt);
-        data_cnt++;
-      end else data_in = 0;
+       if (data_in_ce0 ) begin
+          data_buf[data_cnt]=data_in;
+          data_cnt++;
+        end
+      end
     end else if ( data_cnt >=1 ) begin
       start = 0; //hold data in as is data_in = 0;
       if ( ready ) begin
         data_cnt = 0 ;
       end
     end
+  end
 end
 
 always @(posedge clk )
-  $display("Wave : Start %b, ready %b, idle %b, done %b, data_in %d", start, ready, idle, done, data_in); 
+  $display("Wave : Strt %b, rdy %b, idle %b, done %b, din %4d, din CE %b, dout0 %4d, dout0ce %b, dout0we %b, dout0adr %d, dout1 %d, dout1ce %b d1we %b, d1addr %d ", start, ready, idle, done, data_in,data_in_ce0, data_out_d0, data_out_ce0, data_out_we0, data_out_address0, data_out_d1, data_out_ce1,data_out_we1, data_out_address1); 
 
 initial begin
   result = 1'bx ;
@@ -59,24 +68,23 @@ initial begin
   repeat ( 1 ) @ ( posedge clk ) ;
   start = 1 ;
 
-  for ( int i = 0 ; i < 30*TAPS ; i ++ ) begin
+  for ( int i = 0 ; i < 9*TAPS ; i ++ ) begin
     repeat ( 1 ) @ ( posedge clk ) ;
     if ( done ) begin
-      expected_data_d = expected_data;
-      expected_data = filter.sum()/TAPS;
-      $display("Start %b, ready %b, idle %b, done %b", start, ready, idle, done);
-      if ( cnt < IGNORE_FIRST_N ) begin
-        cnt++;
-        continue;
+      expected_data[0] = data_buf.sum()/TAPS;
+      foreach(data_buf[i]) begin
+        data_buf[i] = (data_buf[i] -expected_data[0])**2;
       end
+      expected_data[1] = data_buf.sum()/TAPS;
+      $display("Start %b, ready %b, idle %b, done %b", start, ready, idle, done);
       cnt++;
-      if ( data_out === expected_data_d ) begin
-        $display ( "P - output data %d expected data %d" , data_out , expected_data_d ) ;
+      if ( (data_out_d0 === expected_data[0]) && (data_out_d1 === expected_data[1]) ) begin
+        $display ( "P - output data %d output data %d expected data %d expected data %d" , data_out_d0, data_out_d1 , expected_data[0], expected_data[1]) ;
         if ( result !== 0 ) result = 1;
       end else begin
-        $display ( "F - output data %d expected data %d" , data_out , expected_data_d ) ;
-        foreach(filter[i]) begin
-          $write("Filter data %d ",filter[i]);
+        $display ( "F - output data %d output data %d expected data %d expected data %d" , data_out_d0 , data_out_d1, expected_data[0], expected_data[1] ) ;
+        foreach(data_buf[i]) begin
+          $write("Buf data %d ",data_buf[i]);
         end
         $display();
         result = 0 ;
@@ -97,7 +105,7 @@ mean_variance mv (
  .ap_ready(ready),
  .data_in_address0,
  .data_in_ce0,
- .data_in_q0,
+ .data_in_q0(data_in),
  .data_out_address0,
  .data_out_ce0,
  .data_out_we0,
