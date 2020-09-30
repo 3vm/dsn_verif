157e68fc789d66c207c847af9d6f529aebcec460
po <3vm@dontemail.com>
2020-09-30 16:00:39 +0530

    bug - input bytes misaligned with function expected format

--- hls/longest_common_subsequence/tb.sv
+++ hls/longest_common_subsequence/tb.sv
@@ -4,13 +4,15 @@ import thee_utils_pkg :: * ;
 
 parameter DWIDTH = 32 ;
 parameter N = 7, M=6;
-parameter NUM_VECTORS=3;
+parameter NUM_OUTPUTS=10;
 
 bit clk ;
 logic rstn ;
-logic [ DWIDTH-1 : 0 ] data_in,data_in_dly ;
-logic [ DWIDTH-1 : 0 ] expected_data[2];
-logic [ DWIDTH-1 : 0 ] data_buf [N+M+1];
+byte data_in ;
+string expected_data  = "TCTA";
+string data_buf; //byte data_buf [N+M+2];
+string seq0 = "ATCTGAT" ;//byte seq0[N+1] ="ATCTGAT" ;
+string seq1 = "TGCATA" ; //byte seq1[M+1] ="TGCATA" ;
 logic start, done, idle, ready;
 int cnt=0;
 int data_cnt=0;
@@ -20,7 +22,7 @@ logic result;
 logic  [3:0] data_in_address0;
 logic   data_in_ce0, data_in_ce0_d;
 logic  [7:0] data_in_q0;
-logic  [3:0] data_out_address0;
+logic  [2:0] data_out_address0;
 logic   data_out_ce0;
 logic   data_out_we0;
 logic  [7:0] data_out_d0;
@@ -28,35 +30,29 @@ logic  [7:0] data_out_d0;
 thee_clk_gen_module #(.FREQ(100)) clk_gen_i0 ( .clk ( clk ) ) ;
 
 initial begin
-  data_in=0;
-  forever @(posedge clk )
-    if ( data_in_ce0 )
-       data_in = $urandom_range(100) ;
+  n = seq0.len(); m=seq1.len();
+  $display("Length of sequence 0 = %d and sequence 1 = %d",n,m);
+  data_buf = { seq0, seq1 };
+  $display("%s",data_buf);
 end
+
 
 initial begin
-  data_in=0; data_in_ce0_d= 0 ;
-  forever @(posedge clk ) begin
-    data_in_ce0_d = data_in_ce0; 
-    if (data_cnt< 4) begin
-      start =  1;
-      if ( !ready && !idle ) begin
-       if (data_in_ce0 ) begin
-          data_buf[data_cnt]=data_in;
-          data_cnt++;
-        end
-      end
-    end else if ( data_cnt >=1 ) begin
-      start = 0; //hold data in as is data_in = 0;
-      if ( ready ) begin
-        data_cnt = 0 ;
-      end
-    end
-  end
+  int cnt = 0 ;
+  data_in=data_buf.getc(cnt);
+  forever @(posedge clk )
+    if ( data_in_ce0 )
+      data_in=data_buf.getc(cnt++);
 end
 
-always @(posedge clk )
-  $display("Wave : Strt %b, rdy %b, idle %b, done %b, din %4d, din CE %b, dout0 %2c, dout0ce %b, dout0we %b, dout0adr %d ", start, ready, idle, done, data_in,data_in_ce0, data_out_d0, data_out_ce0, data_out_we0, data_out_address0 ); 
+always @(posedge clk ) begin
+  if ( data_in inside {"A","T","C","G"}) 
+    $display("Wave : Strt %b, rdy %b, idle %b, done %b, din %2c, din CE %b, din addr %d, dout0 %2c, dout0ce %b, dout0we %b, dout0adr %d ", 
+      start, ready, idle, done, data_in,data_in_ce0, data_in_address0, data_out_d0, data_out_ce0, data_out_we0, data_out_address0 ); 
+  else
+    $display("Wave : Strt %b, rdy %b, idle %b, done %b, din %2d, din CE %b, din addr %d, dout0 %2c, dout0ce %b, dout0we %b, dout0adr %d ", 
+      start, ready, idle, done, data_in,data_in_ce0,  data_in_address0, data_out_d0, data_out_ce0, data_out_we0, data_out_address0 ); 
+end
 
 initial begin
   result = 1'bx ;
@@ -65,25 +61,17 @@ initial begin
   repeat ( 1 ) @ ( posedge clk ) ;
   start = 1 ;
 
-  for ( int i = 0 ; i < NUM_VECTORS ;) begin
+  for ( int i = 0 ; i < NUM_OUTPUTS ; ) begin
     repeat ( 1 ) @ ( posedge clk ) ;
-    if ( done ) begin
-       i ++ ;
-      expected_data[0] = data_buf.sum()/N;
-      show_buf();
-      foreach(data_buf[i]) begin
-        data_buf[i] = (data_buf[i] -expected_data[0])**2;
-      end
-      show_buf();
-      expected_data[1] = data_buf.sum()/N;
-      cnt++;
-      if ( data_out_d0 === expected_data[0] ) begin
+    if ( data_out_ce0 && data_out_we0 ) begin
+      if ( data_out_d0 === expected_data.getc(i) ) begin
         $display ( "P - output data %d expected data %d expected data %d" , data_out_d0, expected_data[0], expected_data[1]) ;
         if ( result !== 0 ) result = 1;
       end else begin
         $display ( "F - output data %d expected data %d expected data %d" , data_out_d0 , expected_data[0], expected_data[1] ) ;
         result = 0 ;
       end
+      i++;
     end
   end
   
