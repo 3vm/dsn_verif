f1f60b523fc47c9bd0ecc2fa93a503c38ccea360
po <3vm@dontemail.com>
2020-10-25 06:14:09 +0530

    bug - duty cycle reported as 1.5 when 0.5 expected

--- thee/thee_clk_duty_meter.sv
+++ thee/thee_clk_duty_meter.sv
@@ -7,7 +7,7 @@ module thee_clk_duty_meter
   timeunit 1ns;
   timeprecision 1ps;
 
-  realtime first_rise_edge, second_rise_edge;
+  realtime first_rise_edge, fall_edge, second_rise_edge;
   real period_in_seconds;
   real sum_of_periods;
 
@@ -16,14 +16,19 @@ module thee_clk_duty_meter
       sum_of_periods = 0 ;
       @(posedge clk);
       first_rise_edge = $realtime();
+      @(negedge clk);
+      fall_edge = $realtime();
       repeat (MEAS_WINDOW) @(posedge clk) begin
         second_rise_edge = $realtime();
 
         period_in_seconds = (second_rise_edge - first_rise_edge) * 1e-9;
-        sum_of_periods += period_in_seconds;
+     //   sum_of_periods += period_in_seconds;
+        @(negedge clk);
+        fall_edge = $realtime();
+        duty = (fall_edge - first_rise_edge) / (second_rise_edge - first_rise_edge ) ;
         first_rise_edge = second_rise_edge;
       end
-      duty = 1.0 / ( sum_of_periods / MEAS_WINDOW );
+//      duty = 1.0 / ( sum_of_periods / MEAS_WINDOW );
     end
   end
   logic vikram;
