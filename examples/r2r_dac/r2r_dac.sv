e272580cd955ee99315d2e36dbac83797a21dd06
po <3vm@dontemail.com>
2021-01-20 07:25:19 +0530

    bug - infinite recursion

--- examples/r2r_dac/r2r_dac.sv
+++ examples/r2r_dac/r2r_dac.sv
@@ -14,38 +14,56 @@ timeunit 1ns ;
 timeprecision 100ps ;
 
 parameter RES_CNT = 2 ** WIDTH ;
- parameter real R_FB = UNIT_R;
-parameter real VREF=1.0;
+ parameter real R_FB = UNIT_R ;
+parameter real VREF = 1.0 ;
 import thee_utils_pkg :: urand_range_real ;
 
 logic [ RES_CNT-1 : 0 ] switch_sel ;
-real v_sources [ WIDTH-1 : 0 ] ;
-real r_series [ RES_CNT-1 : 0 ] = '{default:UNIT_R};
-real r_source [ RES_CNT-1 : 0 ] = '{default:2*UNIT_R};
+real v_eff [ WIDTH-1 : 0 ] ;
+real r_series [ RES_CNT-1 : 0 ] = '{ default : UNIT_R } ;
+real r_source [ RES_CNT-1 : 0 ] = '{ default : 2 * UNIT_R } ;
 real r_eff [ RES_CNT-1 : 0 ] ;
 
 initial begin
-  r_series[0]=2*UNIT_R;
-  for ( int i=WIDTH-1;i>=0;i--) begin
-   r_eff[i]=get_source_r(i,WIDTH-1,UNIT_R);
-  end
+   r_series [ 0 ] = 2 * UNIT_R ;
+   for ( int i = WIDTH-1 ; i >= WIDTH-1 ; i-- ) begin
+     thev_reduce ( i, WIDTH-1, r_eff[i], v_eff[i]);
+   end
 end
 
-always @(dig) begin
-  ana=0;
-  foreach (dig[i])
-	ana += VREF * R_FB / r_eff[i];
+always @ ( dig ) begin
+   ana = 0 ;
+   foreach ( dig [ i ] )
+     ana += dig[i]?v_eff [ i ] : 0;
 end
 
 logic vikram ;
 
-endmodule
+//https://www.allaboutcircuits.com/technical-articles/voltage-mode-r2r-dacs-operation-and-characteristics/
+function automatic void thev_reduce ( input int vindex , input int nodeindex , inout real reff, inout real vthev ) ;
+	real vbranch;
+	real vthev_prev, reff_prev;
+ $display ( "Vi %d ri %d r %f, v thevenin %f" , vindex , nodeindex , reff, vthev ) ;
+if ( nodeindex == 0 ) begin
+    reff = get_eff_r_for_parallel (r_series[0],r_source[0]);
+    vthev = (vindex == 0) ? VREF * r_series[0]/(r_series[0]+r_source[0]) : 0 ;
+ end
+	if (vindex!=nodeindex) begin
+		vbranch = 0 ;
+		thev_reduce ( vindex, nodeindex-1, reff_prev, vthev_prev);
+		vthev = vthev_prev * r_source[nodeindex]/(reff_prev+r_series[nodeindex]+r_source[nodeindex]) ;
+		reff = get_eff_r_for_parallel ( r_source[nodeindex], r_series[nodeindex]+ reff_prev);
+	end else begin
+		vbranch = VREF ;
+		thev_reduce ( vindex, nodeindex-1, reff_prev, vthev_prev);
+		vthev = vbranch * (reff_prev+r_series[nodeindex])/(reff_prev+r_series[nodeindex]+r_source[nodeindex]) ;
+		reff = get_eff_r_for_parallel ( r_source[nodeindex], r_series[nodeindex]+ reff_prev);
+	end
 
-function automatic real get_source_r ( input int vindex, input int rindex, input real rseries );
-  if ( rindex == 0 ) begin
-    return rseries;
-  end else begin
-    get_source_r ( vindex, rindex-1, rseries );
-  end
+ endfunction
+
+function automatic real get_eff_r_for_parallel ( input real r0 , input real r1 ) ;
+  return ( r0 * r1 / ( r0 + r1 ) ) ;
 endfunction
 
+endmodule
--- examples/r2r_dac/tb.sv
+++ examples/r2r_dac/tb.sv
@@ -5,9 +5,9 @@ timeunit 1ns ;
 timeprecision 1ps ;
 
 parameter WIDTH = 4 ;
-parameter real VREF = 1.0;
-real ana , expected;
-real fullscale;
+parameter real VREF = 1.0 ;
+real ana , expected ;
+real fullscale ;
 logic rstn ;
 logic clk ;
 logic [ WIDTH-1 : 0 ] dig ;
@@ -29,14 +29,14 @@ r2r_dac # ( .WIDTH ( WIDTH ) ) r_string_dac
 
 initial begin
    import thee_utils_pkg :: urand_range_real ;
-   fullscale = VREF*(2**WIDTH -1) / 2**WIDTH;
+   fullscale = VREF * ( 2 ** WIDTH -1 ) / 2 ** WIDTH ;
    rstn = 0 ; repeat ( 2 ) @ ( posedge clk ) ; rstn = 1 ;
   
    repeat ( 10 ) @ ( posedge clk ) ;
   
    for ( int i = 0 ; i < 5 ; i ++ ) begin
-     dig = $urandom_range(2**WIDTH-1);
-     expected = fullscale * dig / (2**WIDTH-1) ;
+     dig = $urandom_range ( 2 ** WIDTH-1 ) ;
+     expected = fullscale * dig / ( 2 ** WIDTH-1 ) ;
      repeat ( 1 ) @ ( posedge clk ) ;
      check_result ;
    end
@@ -47,7 +47,7 @@ end
 task check_result ;
  import thee_utils_pkg :: compare_real_fixed_err ;
  $display ( "Analog output %f , Digital input %d , expected analog %f" , ana , dig , expected ) ;
- compare_real_fixed_err ( .expected ( expected ) , .actual ( ana ) , .result ( result ) , .max_err ( 1.002/(2**WIDTH) ) ) ;
+ compare_real_fixed_err ( .expected ( expected ) , .actual ( ana ) , .result ( result ) , .max_err ( 1.002 / ( 2 ** WIDTH ) ) ) ;
  if ( result )
  $display ( "PASS" ) ;
  else begin
