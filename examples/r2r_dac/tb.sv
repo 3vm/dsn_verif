647c0390fe1e120c51a551b986365ea1d6ac3a4a
po <3vm@dontemail.com>
2021-01-26 08:21:41 +0530

    remove clk

--- examples/r2r_dac/tb.sv
+++ examples/r2r_dac/tb.sv
@@ -5,20 +5,14 @@ timeunit 1ns ;
 timeprecision 1ps ;
 
 parameter WIDTH = 4 ;
-parameter real VREF = 1.0 ;
-real ana , expected ;
-real fullscale ;
+parameter real VREF = 1.0;
+real ana , expected;
+real fullscale;
 logic rstn ;
 logic clk ;
 logic [ WIDTH-1 : 0 ] dig ;
 real dig_out_real ;
 bit result ;
-
-parameter OVERSAMP_RATIO = 256 ;
-parameter real FREQ_CLK_OVERSAMP = 256 ;
-
-thee_clk_gen_module # ( .FREQ ( FREQ_CLK_OVERSAMP / OVERSAMP_RATIO ) ) clk_gen ( .clk ( clk ) ) ;
-thee_clk_gen_module # ( .FREQ ( FREQ_CLK_OVERSAMP ) ) clk_gen_oversamp ( .clk ( clk_oversamp ) ) ;
 
 r2r_dac # ( .WIDTH ( WIDTH ) ) r_string_dac
  (
@@ -29,15 +23,12 @@ r2r_dac # ( .WIDTH ( WIDTH ) ) r_string_dac
 
 initial begin
    import thee_utils_pkg :: urand_range_real ;
-   fullscale = VREF * ( 2 ** WIDTH -1 ) / 2 ** WIDTH ;
-   rstn = 0 ; repeat ( 2 ) @ ( posedge clk ) ; rstn = 1 ;
-  
-   repeat ( 10 ) @ ( posedge clk ) ;
+   fullscale = VREF*(2**WIDTH -1) / 2**WIDTH;
   
    for ( int i = 0 ; i < 5 ; i ++ ) begin
-     dig = $urandom_range ( 2 ** WIDTH-1 ) ;
-     expected = fullscale * dig / ( 2 ** WIDTH-1 ) ;
-     repeat ( 1 ) @ ( posedge clk ) ;
+     dig = $urandom_range(2**WIDTH-1);
+     expected = fullscale * dig / (2**WIDTH-1) ;
+     #0;
      check_result ;
    end
   
@@ -47,7 +38,7 @@ end
 task check_result ;
  import thee_utils_pkg :: compare_real_fixed_err ;
  $display ( "Analog output %f , Digital input %d , expected analog %f" , ana , dig , expected ) ;
- compare_real_fixed_err ( .expected ( expected ) , .actual ( ana ) , .result ( result ) , .max_err ( 1.002 / ( 2 ** WIDTH ) ) ) ;
+ compare_real_fixed_err ( .expected ( expected ) , .actual ( ana ) , .result ( result ) , .max_err ( 1.002/(2**WIDTH) ) ) ;
  if ( result )
  $display ( "PASS" ) ;
  else begin
