606d9df0972adfc5d99e60ca93c6d775d00b235f
po <3vm@dontemail.com>
2021-01-28 13:47:01 +0530

    timescale effects from compilation order

--- quiz/q48.sv
+++ quiz/q48.sv
@@ -1,3 +1,3 @@
-`include q48_0.sv
-`include q48_1.sv
+`include "q48_0.sv"
+`include "q48_1.sv"
 `include "q48_tb.sv"
--- quiz/q48_0.sv
+++ quiz/q48_0.sv
@@ -1,7 +1,7 @@
-`timescale 10ns/1ns
+`timescale 1ns/1ns
 
 module myclk0 (output bit clk);
-  always #1 clk0 ^= 1;
+  always #1 clk ^= 1;
   logic vikram;
 endmodule
 
