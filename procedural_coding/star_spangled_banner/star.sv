1965ab5dd60c3a6ca34d14b944c5afd8427584eb
po <3vm@dontemail.com>
2022-05-15 08:27:03 +0530

    bug - still not updating wave data, fixed reading writing to same file descriptor

--- procedural_coding/star_spangled_banner/star.sv
+++ procedural_coding/star_spangled_banner/star.sv
@@ -31,17 +31,17 @@ end
 
 initial begin
   string this_str,swara,window;
-  int fid;
+  int fid_rd, fid_wr;
   int code;
   real sin_fixed_arg, freq,dat ;
   int phase;
   create_swara_freq_table();
   show_swaras;
   write_wave_header(song_wave_file);
-  open_wave_for_data(song_wave_file,fid);
-  fid=$fopen("star-spangled-banner-carnatic.txt","r");
-  code=$fgets(this_str,fid); //comment line
-  while($fscanf(fid,"%s",this_str)!=-1) begin
+  open_wave_for_data(song_wave_file,fid_wr);
+  fid_rd=$fopen("star-spangled-banner-carnatic.txt","r");
+  code=$fgets(this_str,fid_rd); //comment line
+  while($fscanf(fid_rd,"%s",this_str)!=-1) begin
     $display(this_str);
     if (this_str[1]==",") begin
       swara = this_str.substr(0,0);
@@ -69,10 +69,11 @@ initial begin
   // phase of the sine wave from previous note to this note should be continuous to avoid hearing clicks between notes
   //thissteps = c((1+n*(i-1)):(i*n));
   for(int i=0, phase=0;i<n;i++) begin
-    dat = $sin(sin_fixed_arg*phase)*w[phase%n]; //suspect formula - attempting to get continuous phase
+    dat = $sin(sin_fixed_arg*phase)*w[i]; //suspect formula - attempting to get continuous phase
     phase = phase+1; //bug - need some time for overflow handling
+    write_wave_data(fid_wr, dat);
   end
-  write_wave_data(fid, dat);
-
-$fclose(fid);
+  
+  $fclose(fid_rd);
+  $fclose(fid_wr);
 end
