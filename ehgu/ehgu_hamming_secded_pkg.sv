261fbe50ac9317833d02f77e88f78423295007a5
po <3vm@dontemail.com>
2022-05-28 22:55:03 +0530

    Done single error correction double error detection code

--- ehgu/ehgu_hamming_secded_pkg.sv
+++ ehgu/ehgu_hamming_secded_pkg.sv
@@ -1,7 +1,7 @@
 package ehgu_hamming_secded_pkg ;
 
-parameter N = 16 ; //7
-parameter K = 11 ; //4
+parameter N = 16 ; // 7
+parameter K = 11 ; // 4
 parameter logic [ N-1 : 0 ] skip = get_parity_positions ( ) ;
 
 function automatic logic [ N-1 : 0 ] get_parity_positions ( ) ;
@@ -110,4 +110,54 @@ function automatic void hamming_dec (
    extract_data ( .code ( code ) , .data ( data ) ) ;
 endfunction
 
+logic vikram ;
+
+ // ---- Single Error Correction Double Error Detection SECDED -----------
+
+function automatic void hamming_secded_enc (
+   output logic [ N : 0 ] code ,
+   input logic [ K-1 : 0 ] data
+   ) ;
+   logic [ N-1 : 0 ] code_hamming ;
+   hamming_enc ( .code ( code_hamming ) , .data ( data ) ) ;
+   code = { ^code_hamming , code_hamming } ;
+endfunction
+
+function automatic void hamming_secded_dec (
+   input logic [ N : 0 ] code ,
+   output logic [ K-1 : 0 ] data ,
+   output logic [ 1 : 0 ] errors
+   ) ;
+  
+   logic [ N-K-1 : 0 ] parity_hamming ;
+   logic parity_secded ;
+   logic [ N-1 : 0 ] code_hamming ;
+   code_hamming = code [ N-1 : 0 ] ;
+   parity_secded = ^code ;
+   calc_parity ( .code ( code_hamming ) , .parity ( parity_hamming ) ) ;
+   // $display ( "Hamming parity %b , SECDED parity %b" , parity_hamming , parity_secded ) ;
+   if ( parity_hamming == 0 && parity_secded == 0 ) begin
+     errors = 0 ;
+   end else if ( parity_hamming == 0 && parity_secded != 0 ) begin
+     errors = 1 ; // error in secded parity
+   end else if ( parity_hamming!= 0 && parity_secded != 0 ) begin
+     errors = 1 ;
+     correct_code ( .code ( code_hamming ) , .parity ( parity_hamming ) ) ;
+   end else if ( parity_hamming!= 0 && parity_secded == 0 ) begin
+     errors = 2 ;
+   end else begin
+     errors = 2 ;
+   end
+   extract_data ( .code ( code_hamming ) , .data ( data ) ) ;
+endfunction
+
+
 endpackage
+
+ /*
+ // readme
+How different is ECC code for memories compared to hamming with parity?
+How to pipeline hamming code with SPEED parameter 0 - no pipeline , 1 - some pipelining , .. , N - higher pipelining
+How to use two or more instances of the hamming package with different N , K values? compilation units , a config hamming package? try it
+
+ */
--- examples/hamming_secded/tb.sv
+++ examples/hamming_secded/tb.sv
@@ -6,9 +6,19 @@ bit result ;
 logic [ N-1 : 0 ] code ;
 logic [ K-1 : 0 ] data_in , data_out ;
 
+logic [ N : 0 ] code_secded ;
+logic [ 1 : 0 ] errors ;
+
 initial begin
-   result=1;
-   repeat ( 100 ) run_once ( ) ;
+   result = 1 ;
+   repeat ( 10 ) run_once ( ) ;
+   if ( result == 1 ) begin
+     $display ( ) ;
+     repeat ( 3 ) $display ( "PASS" ) ;
+   end else begin
+     repeat ( 3 ) $display ( "FAIL" ) ;
+   end
+   repeat ( 1000 ) run_secded_once ( ) ;
    if ( result == 1 ) begin
      $display ( ) ;
      repeat ( 3 ) $display ( "PASS" ) ;
@@ -23,22 +33,58 @@ task run_once ( ) ;
    bit [ N-1 : 0 ] err ;
    data_in = $urandom ( ) ;
    #0 ;
-   $display("============= Vector Start ==============");
-   $display("Data input %b", data_in);
+   $display ( " _________________ Vector Start _________________ " ) ;
+   $display ( "Data input %b" , data_in ) ;
    hamming_enc ( .code ( code ) , .data ( data_in ) ) ;
-   $display("Code %b", code);
+   $display ( "Code %b" , code ) ;
    err = 1 << $urandom_range ( 0 , N-1 ) ;
    $display ( "Insert error %b" , err ) ;
    code ^= err ;
-   $display("Code with error %b", code);
+   $display ( "Code with error %b" , code ) ;
    $display ( "Decoding" ) ;
    hamming_dec ( .code ( code ) , .data ( data_out ) ) ;
-   $display("Data output %b", data_out);
+   $display ( "Data output %b" , data_out ) ;
    this_result = data_in == data_out ;
-   if ( this_result == 0 ) $display ( "Error found in previous data" );
+   if ( this_result == 0 ) $display ( "Error found in previous data" ) ;
    result &= this_result ;
-   $display("============= Vector End   ==============\n");
+   $display ( " _______________ Vector End _________________ \n" ) ;
 endtask // run_once
+
+
+task run_secded_once ( ) ;
+   bit this_result ;
+   bit [ N : 0 ] err ;
+   int inserted_errors ;
+   data_in = $urandom ( ) ;
+   #0 ;
+   $display ( " _________________ SECDED : Vector Start _________________ " ) ;
+   $display ( "Data input %b" , data_in ) ;
+   hamming_secded_enc ( .code ( code_secded ) , .data ( data_in ) ) ;
+   $display ( "Code %b" , code_secded ) ;
+   err = 0 ;
+   for ( int i = 0 ; i < $urandom_range ( 0 , 2 ) ; i ++ ) begin
+     err |= 1 << $urandom_range ( 0 , N ) ;
+   end
+   inserted_errors = $countones ( err ) ;
+   $display ( "Insert error %b , count %2d" , err , inserted_errors ) ;
+   code_secded ^= err ;
+   $display ( "Code with error %b" , code_secded ) ;
+   $display ( "Decoding" ) ;
+   hamming_secded_dec ( .code ( code_secded ) , .data ( data_out ) , .errors ( errors ) ) ;
+   $display ( "Data output %b" , data_out ) ;
+   if ( inserted_errors == 0 ) begin
+     this_result = data_in == data_out ;
+   end else if ( inserted_errors == 1 ) begin
+     this_result = errors == 1 && data_in == data_out ;
+   end else if ( inserted_errors == 2 ) begin
+     this_result = errors == 2 ;
+   end
+   $display ( "errors found during Decoding %2d" , errors ) ;
+
+   if ( this_result == 0 ) $display ( "Error found in previous data" ) ;
+   result &= this_result ;
+   $display ( " _________________ SECDED : Vector End _________________ \n" ) ;
+endtask // run_secded_once
 
 logic vikram ;
 endmodule
