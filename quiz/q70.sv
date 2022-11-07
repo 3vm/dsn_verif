5a1da3b6c4cae59027fea42796d62c940f6863c1
valsid <contact@valsid.in>
2022-11-08 02:35:28 +0530

    bug - prints only once?

--- quiz/q70.sv
+++ quiz/q70.sv
@@ -1,11 +1,22 @@
 `timescale 1ns/1ps
 module tb;
-  bit p,q,r;
+  bit p,q;
   
   initial begin
-    p=10;
+    p=10; q=100;
     #4;
-	
+	p=20; q=200;
+	#10;
   end
   
+  initial begin
+     #2;
+	 force p=0;
+	 force q=0;
+	 #1;
+	 release p;
+  end
+  
+  initial $monitor ("%3d %3d %t",p,q,$stime()); 
+ 
 endmodule
