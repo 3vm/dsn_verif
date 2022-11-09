7caabba7e103581af8cb4c14cc50c5364b7eafe2
valsid <contact@valsid.in>
2022-11-09 15:14:52 +0530

    Check synthesis

--- examples/digital/clk_gating/sim.f
+++ examples/digital/clk_gating/sim.f
@@ -1,15 +1,5 @@
 timescale.sv
-$EHGU_HOME/ehgu/ehgu_config_pkg.sv
-$EHGU_HOME/ehgu/ehgu_basic_pkg.sv
-$EHGU_HOME/ehgu/ehgu_clkdiv.sv
-$EHGU_HOME/ehgu/ehgu_clkgate.sv
-$EHGU_HOME/ehgu/ehgu_cntr.sv
-$EHGU_HOME/ehgu/ehgu_dly.sv
-$EHGU_HOME/ehgu/ehgu_edges.sv
-$EHGU_HOME/ehgu/ehgu_fedge.sv
-$EHGU_HOME/ehgu/ehgu_redge.sv
-$EHGU_HOME/ehgu/ehgu_rst_sync.sv
-
+-f design.f
 $EHGU_HOME/thee/thee_mathsci_consts_pkg.sv
 $EHGU_HOME/thee/thee_utils_pkg.sv
 $EHGU_HOME/thee/thee_clk_gen_module.sv
