683664d094b01e8109864b3385b907d821094ba0
valsid <contact@valsid.in>
2022-11-11 07:31:22 +0530

    bug unknown behavior of event triggering

--- quiz/q80.sv
+++ quiz/q80.sv
@@ -3,26 +3,36 @@ module tb ;
  real ain ;
  logic y ;
  string mode ;
+ event mark;
 initial begin
    mode = "regular" ;
    apply ( ) ;
    mode = "special" ;
    apply ( ) ;
+   $finish;
 end
  special_not i0 ( .in ( ain ) , .out ( y ) , .mode ( mode ) ) ;
 
 task automatic apply ;
+   bit noise_done=0;
+   real old_ain;
+   $display("Not gate mode %s", mode);
+   ->mark;
    #1
    fork
    for ( int i = 0 ; i < 100 ; i ++ ) begin
      ain += 0.01 ;
      #1 ;
+	 old_ain = ain;
    end
    begin
-     wait ( ain == 0.5 ) ;
+     if (noise_done == 0 )  begin
+	 wait ( ain >= 0.5 && old_ain <= 0.5 ) ;
      ain -= 0.2 ;
      #2 ;
      ain += 0.2 ;
+	 noise_done = 1;
+	 end
    end
    join
    for ( int i = 0 ; i < 15 ; i ++ ) begin
@@ -31,10 +41,11 @@ task automatic apply ;
      if ( ain <= 0.01 ) break ;
    end
    #10 ;
+    ->mark;
 endtask
 
-initial $monitor ( "Time : %6d , Output %b" , $stime , y ) ;
-initial $monitor ( "Time : %6d , Input %1.3f" , $stime , ain ) ;
+initial forever @(y or mark.triggered() )  $display ( "Time : %6d , Input %1.3f, Output %b" , $stime , ain, y ) ;
+//initial $monitor ( "Time : %6d , Input %1.3f" , $stime , ain ) ;
 endmodule
 
 module special_not # ( parameter real P0 = 0.2 , P1 = 0.8 ) (
