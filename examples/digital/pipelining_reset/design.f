2e8acaf3eb358bf8a29a080a4fa912617b9d16de
valsid <contact@valsid.in>
2022-11-14 11:45:51 +0530

    add synthesis script

---
+++ examples/digital/pipelining_reset/design.f
@@ -0,0 +1,11 @@
+$EHGU_HOME/ehgu/ehgu_config_pkg.sv
+$EHGU_HOME/ehgu/ehgu_basic_pkg.sv
+$EHGU_HOME/ehgu/ehgu_clkdiv_fractional.sv
+$EHGU_HOME/ehgu/ehgu_cntr.sv
+$EHGU_HOME/ehgu/ehgu_dly.sv
+$EHGU_HOME/ehgu/ehgu_edges.sv
+$EHGU_HOME/ehgu/ehgu_fedge.sv
+$EHGU_HOME/ehgu/ehgu_redge.sv
+$EHGU_HOME/ehgu/ehgu_rst_sync.sv
+$EHGU_HOME/ehgu/ehgu_rst_tree_piped.sv
+
