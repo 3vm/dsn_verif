46d06b3a395c60255348d9c1dc9e5f98a62bbade
valsid <contact@valsid.in>
2024-11-27 07:12:52 +0530

    Bug: one fixed in merge of 2 and 2or1 long arrays, more merge bug remains

--- examples/software_like/merge_sort/tb.sv
+++ examples/software_like/merge_sort/tb.sv
@@ -1,4 +1,5 @@
 import thee_utils_pkg :: * ;
+util_tasks_c # ( .disp_type ( "int" ) , .SIZE ( 9 ) ) util ;
 
 module tb ;
 timeunit 1ns ;
@@ -6,7 +7,6 @@ timeprecision 100ps ;
 
 
 int tv [ 9 ] = '{ 7 , 15 , 16 , 1 , 8 , 7 , 6 , 4 , 10 } ;
-util_tasks_c # ( .disp_type ( "int" ) , .SIZE ( 9 ) ) util ;
 
 initial begin
    $display ( "Input unsorted data" ) ;
@@ -21,12 +21,8 @@ endmodule
 function automatic void merge_sort ( ref int arr [ 9 ] , input int low , high ) ;
   int low_left , low_right ;
   int high_left , high_right ;
-  util_tasks_c # ( .disp_type ( "int" ) , .SIZE ( 9 ) ) util ;
+
   if ( high -low <= 0 ) begin
-     return ;
-  end else if ( high -low == 1 ) begin
-     if ( arr [ high ] > arr [ low ] )
-     swap ( arr [ low ] , arr [ high ] ) ;
      return ;
   end
   
@@ -37,16 +33,19 @@ function automatic void merge_sort ( ref int arr [ 9 ] , input int low , high ) 
   
   merge_sort ( .arr ( arr ) , .low ( low_left ) , .high ( high_left ) ) ;
   merge_sort ( .arr ( arr ) , .low ( low_right ) , .high ( high_right ) ) ;
-  
+
+  $display("Before merge");
+  util.arr_print ( arr ) ;
+  $display ( "Merging left: %3d %3d with right: %3d %3d" , low_left , high_left , low_right , high_right ) ;
+
   merge ( .arr ( arr ) , .low_left ( low_left ) , .high_left ( high_left ) , .low_right ( low_right ) , .high_right ( high_right ) ) ;
-	util.arr_print ( arr ) ;
+  $display("After merge");
+  util.arr_print ( arr ) ;
 endfunction
 
 function automatic void split_arr ( ref int arr [ 9 ] , input int low , high , output int low_left , low_right , high_left , high_right ) ;
-
-   low_left = low ;
-   high_right = high ;
-  
+  low_left = low ;
+  high_right = high ;
   if ( high - low == 0 ) begin
      high_left = low ;
      low_right = high ;
@@ -57,19 +56,16 @@ function automatic void split_arr ( ref int arr [ 9 ] , input int low , high , o
      high_left = ( low + high ) / 2 ;
      low_right = high_left + 1 ;
   end
- 
 endfunction
 
 function automatic void merge ( ref int arr [ 9 ] , input int low_left , low_right , high_left , high_right ) ;
-  int i , j ;
-  i = low_left ;
-  j = low_right ;
-  for ( int i = low_left , j = low_right ; i <= low_right && j <= high_right ; ) begin
+  for ( int i = low_left , j = low_right ; (i <= high_left) && (j <= high_right) ; ) begin
+     $display ("Arr location, value left %d %d, Right %d  %d", i, arr[i], j, arr[j]);
      if ( arr [ i ] > arr [ j ] ) begin
        swap ( arr [ i ] , arr [ j ] ) ;
-       i ++ ;
-     end else begin
        j ++ ;
+     end else begin
+       i ++ ;
     end
   end
 endfunction
