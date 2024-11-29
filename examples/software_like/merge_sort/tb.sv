7bc8a5b9139cc4d3f7a2b705e4165d019a1352d5
valsid <contact@valsid.in>
2024-11-29 06:57:32 +0530

    Bug: Sorting is wrong, merging is happening on unsorted data

--- examples/software_like/merge_sort/tb.sv
+++ examples/software_like/merge_sort/tb.sv
@@ -1,4 +1,4 @@
-import thee_utils_pkg :: * ;
+import thee_utils_pkg :: util_tasks_c ;
 util_tasks_c # ( .disp_type ( "int" ) , .SIZE ( 9 ) ) util ;
 
 module tb ;
@@ -7,21 +7,22 @@ timeprecision 100ps ;
 
 
 int tv [ 9 ] = '{ 7 , 15 , 16 , 1 , 8 , 7 , 6 , 4 , 10 } ;
+int sorted [ 9 ] ;
 
 initial begin
    $display ( "Input unsorted data" ) ;
    util.arr_print ( tv ) ;
-   merge_sort ( .arr ( tv ) , .low ( 0 ) , .high ( 9-1 ) ) ;
+   merge_sort ( .arr ( tv ) , .low ( 0 ) , .high ( 9-1 ) , .sorted ( sorted ) ) ;
    $display ( "Output sorted data" ) ;
-   util.arr_print ( tv ) ;
+   util.arr_print ( sorted ) ;
 end
 
 endmodule
 
-function automatic void merge_sort ( ref int arr [ 9 ] , input int low , high ) ;
+function automatic void merge_sort ( ref int arr [ 9 ] , input int low , high , ref int sorted [ 9 ] ) ;
    int low_left , low_right ;
    int high_left , high_right ;
-  
+                                     
    if ( high -low <= 0 ) begin
      return ;
    end
@@ -31,19 +32,20 @@ function automatic void merge_sort ( ref int arr [ 9 ] , input int low , high ) 
    .low_right ( low_right ) , .high_right ( high_right ) ) ;
    $display ( "%3d %3d %3d %3d" , low_left , high_left , low_right , high_right ) ;
   
-   merge_sort ( .arr ( arr ) , .low ( low_left ) , .high ( high_left ) ) ;
-   merge_sort ( .arr ( arr ) , .low ( low_right ) , .high ( high_right ) ) ;
+   merge_sort ( .arr ( arr ) , .low ( low_left ) , .high ( high_left ) , .sorted ( sorted ) ) ;
+   merge_sort ( .arr ( arr ) , .low ( low_right ) , .high ( high_right ) , .sorted ( sorted ) ) ;
   
    $display ( "Before merge" ) ;
    util.arr_print ( arr ) ;
    $display ( "Merging left : %3d %3d with right : %3d %3d" , low_left , high_left , low_right , high_right ) ;
   
-   merge ( .arr ( arr ) , .low_left ( low_left ) , .high_left ( high_left ) , .low_right ( low_right ) , .high_right ( high_right ) ) ;
+   merge ( .arr ( arr ) , .low_left ( low_left ) , .high_left ( high_left ) , .low_right ( low_right ) , .high_right ( high_right ) , .sorted ( sorted ) ) ;
    $display ( "After merge" ) ;
-   util.arr_print ( arr ) ;
+   util.arr_print ( sorted ) ;
 endfunction
 
 function automatic void split_arr ( ref int arr [ 9 ] , input int low , high , output int low_left , low_right , high_left , high_right ) ;
+  $display ( "Splitting %3d to %3d" , low , high ) ;
    low_left = low ;
    high_right = high ;
    if ( high - low == 0 ) begin
@@ -58,35 +60,23 @@ function automatic void split_arr ( ref int arr [ 9 ] , input int low , high , o
    end
 endfunction
 
-function automatic void merge ( ref int arr [ 9 ] , input int low_left , low_right , high_left , high_right ) ;
-   int tmp [ 9 ] ;
-   tmp = arr ;
-   for ( int k = low_left , i = low_left , j = low_right ; k <= high_right ; k++) begin
+function automatic void merge ( ref int arr [ 9 ] , input int low_left , low_right , high_left , high_right , ref int sorted [ 9 ] ) ;
+   for ( int k = low_left , i = low_left , j = low_right ; k <= high_right ; k ++ ) begin
      if ( i <= high_left && j <= high_right ) begin
        $display ( "Comparing locations %d and %d" , i , j ) ;
        if ( arr [ i ] > arr [ j ] ) begin
-         // $display ( "swaping locations %d and %d" , i , j ) ;
-         // swap ( arr [ i ] , arr [ j ] ) ;
-         tmp [ k ] = arr [ j ] ;
+         sorted [ k ] = arr [ j ] ;
          j ++ ;
        end else begin
-         tmp [ k ] = arr [ i ] ;
+         sorted [ k ] = arr [ i ] ;
          i ++ ;
        end
      end else if ( i <= high_left ) begin
-         tmp [ k ] = arr [ i ] ;
-         i ++ ;
+       sorted [ k ] = arr [ i ] ;
+       i ++ ;
      end else begin
-         tmp [ k ] = arr [ j ] ;
-         j ++ ;
+       sorted [ k ] = arr [ j ] ;
+       j ++ ;
      end
    end
-   arr = tmp ;
-endfunction
-
-function automatic void swap ( ref int a , b ) ;
-   int tmp ;
-   tmp = a ;
-   a = b ;
-   b = tmp ;
 endfunction
