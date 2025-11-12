004f746c8c1d938722ada77fd671fd3b411a4542
valsid <contact@valsid.in>
2025-11-12 13:45:45 +0530

    fixes to path changes of Vivado binaries

--- utils/run-quiz.sh
+++ utils/run-quiz.sh
@@ -1,4 +1,4 @@
-~/xilinx/Vivado/2022.1/bin/xvlog --sv --work work $1 $2
-~/xilinx/Vivado/2022.1/bin/xelab -mt 4 --snapshot work tb
-~/xilinx/Vivado/2022.1/bin/xsim work --runall -sv_seed random
+$VIVADO_HOME/xvlog --sv --work work $1 $2
+$VIVADO_HOME/xelab -mt 4 --snapshot work tb
+$VIVADO_HOME/xsim work --runall -sv_seed random
 
--- utils/run.sh
+++ utils/run.sh
@@ -1,4 +1,5 @@
-set VIVADO_HOME=/tools/Xilinx/2025.1/Vivado/bin/
+
+#set VIVADO_HOME=/tools/Xilinx/2025.1/Vivado/bin/
 $VIVADO_HOME/xvlog --incr --sv --work work -f sim.f
 $VIVADO_HOME/xelab -mt 4 --snapshot work tb
 $VIVADO_HOME/xsim work --runall -sv_seed random
