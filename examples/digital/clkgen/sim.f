c480ea407daaefc7753ea80a688540857aa837a7
valsid <contact@valsid.in>
2025-11-26 07:11:30 +0530

    move to verif, its not synthesizable

--- examples/digital/clkgen/sim.f
+++
@@ -1,7 +0,0 @@
-timescale.sv
-$EHGU_HOME/thee/thee_mathsci_consts_pkg.sv
-$EHGU_HOME/thee/thee_utils_pkg.sv
-$EHGU_HOME/thee/thee_clk_freq_meter.sv
-$EHGU_HOME/thee/thee_clk_duty_meter.sv
-$EHGU_HOME/thee/thee_clk_gen_module.sv
-tb.sv
