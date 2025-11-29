dadaafc9ee0ab8b232598beba2f57c48114a1967
valsid <contact@valsid.in>
2025-11-29 07:20:20 +0530

    small change

--- quiz/q84.sv
+++ quiz/q84.sv
@@ -1,22 +1,22 @@
-module tb;
-  logic a,b,c;
-  
-  always @(*) begin
-     c =0 ;
-     if ( a & ~b) 
-        c = a;
-//     else if ( a & b) ;
-     else if ( a & b) 
-        c=b;
-  end
+module tb ;
+ logic a , b , c ;
 
-  initial begin
-     for(int i=0;i<4;i++)  begin
-         {a,b} = i;
-         #0;
-         $display("A = %b , B = %b , C = %b", a,b,c);
-     end
+ always @ ( * ) begin
+   c = 0 ;
+   if ( a & ~b )
+     c = a ;
+   else if ( a & b ) ;
+   //else if ( a & b )
+     c = b ;
+ end
+
+ initial begin
+   for ( int i = 0 ; i < 4 ; i ++ ) begin
+     { a , b } = i ;
+     #0 ;
+     $display ( "A = %b , B = %b , C = %b" , a , b , c ) ;
    end
+ end
 
 endmodule
 
--- quiz/q87.sv
+++ quiz/q87.sv
@@ -9,18 +9,11 @@ bit [9:0] tv = 'b 0101100101 ;
 
 always ck = #1ns ~ck;
 
-//always w = $past (q,3,,@(posedge ck));
-//WARNING: [VRFC 10-1771] potential always loop found [~/Desktop/RTL_design/dsn_verif/quiz/q87.sv:12]
-
 always @(ck)
   x = $past (q,3,,@(posedge ck));
 always @(posedge ck) y = $past (q,3);
 assign  z = $past (q,3,,@(posedge ck));
 
-//initial $monitor("%b",p);   
-//always @(posedge ck) $write(w);
-//always @(posedge ck) $write(x);
-//always @(posedge ck) $write(y);
 initial begin
   $display("Test Vector\n%b", tv);
   $display("Output");
