1c5804e975c9b2aad90b4b066606ce1d901d60c8
valsid <contact@valsid.in>
2025-12-07 15:45:59 +0530

    fix connections

--- examples/chip/vt0/osc_die_top.sv
+++ examples/chip/vt0/osc_die_top.sv
@@ -1,11 +1,8 @@
 module osc_die_top (
 inout VDD,
 inout VSS,
-input CLKIN,
-output VOUT
+input CLKOUT
 );
 pads u_pads ();
-dac_r2r_ana_core u_ana_core();
-dac_r2r_dig_core u_dig_core();
 endmodule
 
