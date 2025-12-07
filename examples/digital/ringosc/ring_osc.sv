d58eea8325f55b473e795d7c59f212414bcaaa0e
valsid <contact@valsid.in>
2025-12-07 16:29:26 +0530

    Bug - fails for 7 inv but passes for 5 inv

--- examples/digital/ringosc/ring_osc.sv
+++ examples/digital/ringosc/ring_osc.sv
@@ -8,15 +8,15 @@ input logic en ,
 output logic clkout
  ) ;
 
-logic [CNT:0] inv_net;
+logic [ CNT : 0 ] inv_net ;
 generate
-  for (genvar i = 0; i<CNT ; i++) begin
-      not #2 i_del ( inv_net[i+1], inv_net[i]);    
-  end
+ for ( genvar i = 0 ; i < CNT ; i ++ ) begin
+   not #2 i_del ( inv_net [ i + 1 ] , inv_net [ i ] ) ;
+ end
 endgenerate
 
-assign clkout = inv_net[CNT];
-assign inv_net[0] = en & inv_net[CNT];
+assign clkout = inv_net [ CNT ] ;
+assign inv_net [ 0 ] = en & inv_net [ CNT ] ;
 
 logic vikram ;
 endmodule
--- examples/digital/ringosc/tb.sv
+++ examples/digital/ringosc/tb.sv
@@ -2,31 +2,32 @@
 module tb ;
 import thee_utils_pkg :: check_approx_equality ;
 logic clkout ;
-real fout;
-real duty;
-bit result;
-logic en;
+real fout ;
+real duty ;
+bit result ;
+logic en ;
 
-thee_clk_freq_meter fmeter0 ( .clk ( clkout ) , .freq_in_hertz ( fout ) ) ;
-thee_clk_duty_meter dmeterr ( .clk ( clkout ) , .duty ( duty ) ) ;
-
-ring_osc osc
+thee_clk_freq_meter fmeter ( .clk ( clkout ) , .freq_in_hertz ( fout ) ) ;
+thee_clk_duty_meter dmeter ( .clk ( clkout ) , .duty ( duty ) ) ;
+localparam CNT = 5 ;
+ring_osc # ( .CNT ( CNT ) ) osc
  (
  .en ,
- .clkout 
+ .clkout
  ) ;
 
 initial begin
-   $display ( " start");
-   #10;
-   en = 0;
-   #10;
-   en = 1; 
-   repeat ( 10 ) @ ( posedge clkout ) ;
+   $display ( " start" ) ;
+   #10 ;
+   en = 0 ;
+   #10 ;
+   en = 1 ;
+   repeat ( 500 ) @ ( posedge clkout ) ;
    $display ( " Clkout frequency %e" , fout ) ;
-   check_approx_equality ( .inp ( fout ) , .expected ( 1.0/12 * 1e9 ) , .result ( result ) ) ;
+   $display ( " Duty Cycle %3.2f " , duty ) ;
+  
+   check_approx_equality ( .inp ( fout ) , .expected ( 1.0 / ( 2 * 2 * CNT ) * 1e9 ) , .result ( result ) ) ;
    if ( result == 1 ) begin
-     $display ( " Duty Cycle - inp clk %3.2f " , duty) ;
      repeat ( 3 ) $display ( "PASS" ) ;
      // TBD - Add automation for duty cycle check
    end else begin
