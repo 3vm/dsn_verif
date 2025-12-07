7f7ea5dec4c38ea9ddeff59c55f5a21d36aebaf0
valsid <contact@valsid.in>
2025-12-07 16:38:15 +0530

    Fixed - enable made clock dependent

--- examples/digital/ringosc/tb.sv
+++ examples/digital/ringosc/tb.sv
@@ -9,7 +9,8 @@ logic en ;
 
 thee_clk_freq_meter fmeter ( .clk ( clkout ) , .freq_in_hertz ( fout ) ) ;
 thee_clk_duty_meter dmeter ( .clk ( clkout ) , .duty ( duty ) ) ;
-localparam CNT = 5 ;
+localparam CNT = 17 ;
+localparam real INV_DEL = 2;
 ring_osc # ( .CNT ( CNT ) ) osc
  (
  .en ,
@@ -20,13 +21,16 @@ initial begin
    $display ( " start" ) ;
    #10 ;
    en = 0 ;
-   #10 ;
+   repeat (100) begin
+      if ( clkout == 0 ) break;
+	  #10;
+	end;
    en = 1 ;
-   repeat ( 500 ) @ ( posedge clkout ) ;
+   repeat ( 50 ) @ ( posedge clkout ) ;
    $display ( " Clkout frequency %e" , fout ) ;
    $display ( " Duty Cycle %3.2f " , duty ) ;
   
-   check_approx_equality ( .inp ( fout ) , .expected ( 1.0 / ( 2 * 2 * CNT ) * 1e9 ) , .result ( result ) ) ;
+   check_approx_equality ( .inp ( fout ) , .expected ( 1.0 / ( 2 * INV_DEL * CNT ) * 1e9 ) , .result ( result ) ) ;
    if ( result == 1 ) begin
      repeat ( 3 ) $display ( "PASS" ) ;
      // TBD - Add automation for duty cycle check
