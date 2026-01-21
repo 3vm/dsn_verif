cb6d04bb5cca6da6b4297cb3fee721c632606b76
valsid <contact@valsid.in>
2026-01-21 15:21:50 +0530

    Bug - RC network not working, save before leaving

--- examples/mixed_signal/rc_phase_shift_osc/fb_network.sv
+++ examples/mixed_signal/rc_phase_shift_osc/fb_network.sv
@@ -17,20 +17,85 @@ parameter real R1 = R , R2 = R , R3 = R ;
 parameter real C1 = C , C2 = C , C3 = C ;
 
 real step ;
-real ir1 , ir2 , ir3 , ic1 , ic2 , ic3 , vc1 , vc2 , vc3 , vn0 , vn1 ;
+real ir1 , ir2 , ir3 , ic1 , ic2 , ic3 , vc1 , vc2 , vc3 , vn0 , vn1,vn2 ;
 
 real vnet1 ;
 
+//R3 not in series , symmetric connection somehow did not work
+
+initial begin
+   vc1 = 0 ; vc2 = 0 ; vc3 = 0 ; 
+   forever begin
+   vout = vin -vc1 -vc2 -vc3;
+
+     ir3 = vout / R3 ;
+	 ic3 = ir3;
+
+     vn1 = vout + vc3 ;
+     ir2 = vn1 / R2 ;	 
+     ic2 = ir2 + ir3 ;
+
+
+     vn0 = vn1 + vc2 ;
+     ir1 = vn0 / R1 ;
+     ic1 = ir1 + ic2 ;
+
+     step = ic1 * TIME_STEP * TIME_STEP_UNIT / C1 ;
+     vc1 += step ;
+     step = ic2 * TIME_STEP * TIME_STEP_UNIT / C2 ;
+     vc2 += step ;
+     step = ic3 * TIME_STEP * TIME_STEP_UNIT / C3 ;
+     vc3 += step ;
+           
+     #TIME_STEP ;
+    
+   end
+end
+
+
+/* Grows unbounded may be because the C1 is updated 3 times per time step , C2 two per tstep c1 once per tstep
+initial begin
+   vc1 = 0 ; vc2 = 0 ; vc3 = 0 ; 
+   forever begin
+   vout = vin -vc1 -vc2 -vc3;
+
+     ir3 = vout / R3 ;
+     step = ic3 * TIME_STEP * TIME_STEP_UNIT / C3 ;
+     vc3 += step ;
+
+     vn1 = vout + vc3 ;
+     ir2 = vn1 / R2 ;	 
+     ic2 = ir2 + ir3 ;
+     step = ic2 * TIME_STEP * TIME_STEP_UNIT / C2 ;
+     vc2 += step ;
+
+
+     vn0 = vn1 + vc2 ;
+     ir1 = vn0 / R1 ;
+     ic1 = ir1 + ic2 ;
+     step = ic1 * TIME_STEP * TIME_STEP_UNIT / C1 ;
+     vc1 += step ;
+           
+     #TIME_STEP ;
+    
+   end
+end
+*/
+
+
+
+
+/*R3 in series
 initial begin
    vc1 = 0 ; vc2 = 0 ; vc3 = 0 ;
-   ir2 = 0 ;
    forever begin
      vn0 = vin - vc1 ;
      vn1 = vn0 - vc2 ;
-     vout = vn1 -vc3 ;
+	 vn2 = vn1 - vc3;
+	 
      ir1 = vn0 / R1 ;
      ir2 = vn1 / R2 ;
-     ir3 = vout / R3 ;
+     ir3 = (vn2 - vout) /R3;	 
     
      ic3 = ir3 ;
      ic2 = ir2 + ic3 ;
@@ -42,22 +107,28 @@ initial begin
      vc2 += step ;
      step = ic3 * TIME_STEP * TIME_STEP_UNIT / C3 ;
      vc3 += step ;
-    
-     vout = ir3 * R3 ;
-    
+
+	 //vout = vn2 - ir3 * R3; //locks up vout because the updates in cap voltages are not seen.
+	 vout = vin - vc1 -vc2 -vc3 -ir3 *R3;
+  
      #TIME_STEP ;
-    
+   
    end
 end
+*/
+
 
 
 initial begin
    forever begin
      #20ps ;
-     // $display ( "Input %1.3f current %1.7e Step %1.7e cap_voltage %1.7e Current time %t" , vin , ic1 , step , vnet1 , $realtime ( ) ) ;
-     // $display ( "Input %1.3f R1 current %1.7e cap_voltage %1.7e Output Voltage %1.7e Current time %t" , vin , ir2 , vnet1 , vout , $realtime ( ) ) ;
+//     $display ( "Input %1.3f R1 current %1.7e V(n0) %1.7e R2 current %1.7e \t V(out) %1.7e  R3 current %1.7e \t time %t" , vin , ir1 , vn0 , ir2 , vout, ir3, $realtime ( ) ) ;
+//     $display ( "Input %1.3f R1 current %1.7e V(n0) %1.7e V(n1) %1.7e \t V(out) %1.7e \t time %t" , vin , vn0 , vn1 , vout, ir3, $realtime ( ) ) ;
+//     $display ( "Input %1.3f V(n0) %1.7e V(C1) %1.7e \t time %t" , vin , vn0 , vc1 , $realtime ( ) ) ;
+     $display ( "Input %1.3f vout %1.7e V(C3) %1.7e ic3 %1.5e \t time %t" , vin , vout , vc3 , ic3, $realtime ( ) ) ;
    end
 end
 
  logic vikram ;
 endmodule
+
--- examples/mixed_signal/rc_phase_shift_osc/tb.sv
+++ examples/mixed_signal/rc_phase_shift_osc/tb.sv
@@ -6,8 +6,10 @@ timeprecision 1ps ;
 parameter real TIMEUNIT_SCALING = 1e-9 ; // keep same as timeunit
 localparam real TIME_CONST_10TO90 = $ln ( 9 ) ;
 
-parameter real R = 1000 , C = 10e-12 ;
-parameter real EXP_FOUT = 1 / ( R * C ) ;
+parameter real R = 100 , C = 10e-12 ;
+import thee_mathsci_consts_pkg :: const_pi ;
+
+parameter real EXP_FOUT = 1 / (2*const_pi* $sqrt(6)* ( R * C ) );
 real vout , fb ;
 logic rstn ;
 logic clk_out ;
@@ -17,7 +19,6 @@ logic result , tresult ;
 import thee_utils_pkg :: save_test_result ;
 import thee_utils_pkg :: update_test_status ;
 
-import thee_mathsci_consts_pkg :: const_pi ;
 import thee_utils_pkg :: check_approx_equality ;
 
 fb_network # ( .R ( R ) , .C ( C ) ) fb_network ( .vin ( fb ) , .vout ( vout ) ) ;
@@ -26,47 +27,41 @@ saturating_amp saturating_amp ( .vin(vout), .vout(fb));
 schmitt_trigger_inv # ( .LT ( 0.1 ) , .UT ( 0.15 ) ) sinv ( .in ( vout ) , .out ( clk_out ) ) ;
 thee_clk_freq_meter # ( .MEAS_WINDOW ( 50 ) ) fmeter ( .clk ( clk_out ) , .freq_in_hertz ( fout ) ) ;
 
- /*
-thee_rc # ( .R ( R ) , .C ( C ) ) rc0
- (
-.vin ( fb ) ,
-.vcap ( net0 )
- ) ;
-
-
-thee_rc # ( .R ( R ) , .C ( C ) ) rc1
- (
-.vin ( net0 ) ,
-.vcap ( net1 )
- ) ;
-
-
-thee_rc # ( .R ( R ) , .C ( C ) ) rc2
- (
-.vin ( net1 ) ,
-.vcap ( vout )
- ) ;
- */
-
 import thee_mathsci_consts_pkg :: const_pi ;
 import thee_utils_pkg :: compare_real_fixed_err ;
 
 initial begin
-   #0.1ns ;
-   inject_noise ;
+   inject_noise (0.01) ; // power on noise may be sufficient
+/*
+   force fb = 0;
+   #1ns;
+
+   force fb = 1;
+   #1ns;
+
+   force fb = 0.5;
+   #1ns;
+*/
    #50ns ;
+  
+   
+  
   
    check_approx_equality ( .inp ( fout ) , .expected ( EXP_FOUT ) , .tolerance ( 0.02 ) , .tolerance_for_zero ( 0.01 ) , .result ( result ) ) ;
    update_test_status ( .result ( tresult ) , .this_result ( result ) ) ;
-   $display ( "Expected frequency %1.3e , Actual frequency %1.3e" , fout , EXP_FOUT ) ;
+   $display ( "Expected frequency %1.3e , Actual frequency %1.3e" , EXP_FOUT , fout ) ;
    save_test_result ( tresult ) ;
-  
-  
+   
    $finish ;
 end
 
-task inject_noise ;
-   force vout = 0.002 ;
+task inject_noise (input real val) ;
+   #0.1ns ;
+   force vout = 0.0 ;
+   #0.1ns ;
+   force vout = val ;
+   #0.1ns ;
+   force vout = 0.0 ;
    #0.1ns ;
    release vout ;
 endtask
@@ -75,7 +70,7 @@ int fd ;
 
 initial begin
    fd = $fopen ( "Wave.dat" , "w" ) ;
-     $fwrite ( fd , "Time, feedback, int net0, int net1, Amplifier out\n" ) ;
+     $fwrite ( fd , "Time, Amp out/fb, int net0, int net1, Amplifier in \n" ) ;
    forever begin
      #0.1ns ;
      $fwrite ( fd , "%e , %e , %e , %e , %e\n" , $realtime ( ) , fb , fb_network.vn0 , fb_network.vn1 , vout ) ;
