ee3094c8883ed87913ecbf1ff84438a6c0c9e0db
valsid <contact@valsid.in>
2026-01-21 15:25:37 +0530

    Formatting only - This task does not work because const ref clk argument is not honored

--- thee/thee_sig_analysis_pkg.sv
+++ thee/thee_sig_analysis_pkg.sv
@@ -1,28 +1,29 @@
-package thee_sig_analysis_pkg;
+package thee_sig_analysis_pkg ;
 
-  timeunit 1ns;
-  timeprecision 100ps;
- 
+ timeunit 1ns ;
+ timeprecision 100ps ;
+
  task automatic get_binary_clk_freq
-  (
-    const ref logic clk,
-    output real freq_in_hertz
-  );
+   (
+//   ref logic clk , -- without const - getting error - ERROR: [VRFC 10-3823] variable 'clk' might have multiple concurrent drivers
+   const ref logic clk ,
+   output real freq_in_hertz
+   ) ;
+  
+   realtime first_rise_edge , second_rise_edge ;
+   real period_in_seconds ;
+   $display ( "In task" ) ;
+  
+   repeat ( 1 ) @ ( posedge clk ) ;
+   first_rise_edge = $realtime ( ) ;
+   $display ( "Started" ) ;
+   @ ( posedge clk ) ;
+   second_rise_edge = $realtime ( ) ;
+  
+   period_in_seconds = ( second_rise_edge - first_rise_edge ) * 1e-9 ;
+   freq_in_hertz = 1.0 / period_in_seconds ;
+   $display ( "Started" ) ;
+ endtask
 
-    realtime first_rise_edge, second_rise_edge;
-    real period_in_seconds;
-      $display ("In task");
-
-    repeat (1) @(posedge clk);
-    first_rise_edge = $realtime();
-  $display ("Started");
-    @(posedge clk);
-    second_rise_edge = $realtime();
-
-    period_in_seconds = (second_rise_edge - first_rise_edge) * 1e-9;
-    freq_in_hertz = 1.0 / period_in_seconds;
-  $display ("Started");
-  endtask
-
-  logic vikram ;
-endpackage 
+ logic vikram ;
+endpackage
