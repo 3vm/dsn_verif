57ef521cb0bf23ef9aaed6a524885581a0334225
valsid <contact@valsid.in>
2026-01-28 16:34:34 +0530

    harder stimulus

--- examples/software_like/num_base_change/tb.sv
+++ examples/software_like/num_base_change/tb.sv
@@ -1,43 +1,33 @@
 program tb ;
-parameter M = 16 , N = 2 ;
-parameter MAX_DIGITS = 20 ;
+
 parameter string symbolsall = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-int symbol_val_map[256];
-byte symbolsm [ M ] , symbolsn [ N ] ;
+int symbol_val_map [ 256 ] ;
+parameter MAX_DIGITS = 20 ;
+
+// parameter M = 16 , N = 2 ;
+// string num_m = "1a" , num_n ;
+
+parameter M = 10 , N = 36 ;
+string num_m = "1024" , num_n ;
 
 
-string num_m = "1a" , num_n ;
 int num ;
 
-function automatic void get_symbol_val_map ( );
-   //assigns integer value to ascii symbols
-   foreach (symbolsall[k]) begin
-      symbol_val_map[symbolsall[k]]=k;
-	  end
-endfunction
-
-
-function automatic void get_base_symbols ( int sel ) ;
-   foreach ( symbolsall [ i ] ) begin
-     // $display ( "%c" , symbolsall [ i ] ) ;
-     if ( sel == 0 ) begin
-       if ( i > M-1 ) break ;
-       symbolsm [ i ] = symbolsall [ i ] ;
-     end else begin
-       if ( i > N-1 ) break ;
-       symbolsn [ i ] = symbolsall [ i ] ;
-     end
+function automatic void get_symbol_val_map ( ) ;
+   // assigns integer value to ascii symbols
+   foreach ( symbolsall [ k ] ) begin
+     symbol_val_map [ symbolsall [ k ] ] = k ;
    end
- endfunction
+endfunction
 
 function automatic int conv_to_int ( string m , int base ) ;
    // $display ( m.len ( ) ) ;
    int i = 0 ;
    int power = 1 ;
-   for ( int j = m.len ( )-1; j>=0 ; j -- ) begin
-     i += power * symbol_val_map[m [ j ] ];
+   for ( int j = m.len ( ) -1 ; j >= 0 ; j -- ) begin
+     i += power * symbol_val_map [ m [ j ] ] ;
      power *= base ;
-	 $display(m[j], i,power);
+     $display ( m [ j ] , i , power ) ;
    end
    return ( i ) ;
 endfunction
@@ -48,32 +38,22 @@ function automatic string conv_from_int ( int i , int base ) ;
    for ( int k = 0 ; k < MAX_DIGITS ; k ++ ) begin
      dig = i%base ;
      i = i / base ;
-     $display ( dig , i ) ;
-     $display ( m ) ;
-//	 $display(symbolsall[dig]);
-	 $display("%c",symbolsall.getc(11));
-     m = { m ,  symbolsall.getc(dig)} ;
-     $display ( m ) ;
+     $display ( dig , i, m ) ;
+     m = { m , symbolsall.getc ( dig ) } ;
      if ( i == 0 ) begin
-	 m = {<<8{m}}; //reverse
-	 break ;
-	 end
+       m = { << 8{ m } } ; // reverse
+       break ;
+     end
    end
    return ( m ) ;
 endfunction
 
 initial begin
-  
-   get_base_symbols ( 0 ) ;
-   get_base_symbols ( 1 ) ;
-   get_symbol_val_map();
+   get_symbol_val_map ( ) ;
    $display ( "Input number %s in base %2d" , num_m , M ) ;
    num = conv_to_int ( num_m , M ) ;
    $display ( "Number in integer %d" , num ) ;
    num_n = conv_from_int ( num , N ) ;
    $display ( "Number in base %d is %s" , N , num_n ) ;
-   // $display ( symbolsall ) ;
-   // $display ( symbolsm ) ;
-   // $display ( symbolsn ) ;
 end
 endprogram
